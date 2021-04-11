ca35baa35dc4ee5d0364cdd18179691eb7599366
Kevin Wang <kevmo314@gmail.com>
2021-04-11 10:07:23 -0400

    add parting

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 import * as tmi from "tmi.js";
 import Bottleneck from "bottleneck";
 import * as process from "process";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 admin.initializeApp();
@@ -53,23 +54,65 @@ async function subscribe(provider: string, channel: string) {
   return false; // not handled by this agent.
 }
 
-const TOPIC = new PubSub().topic("projects/rtchat-47692/topics/subscribe");
-
-TOPIC.subscription("projects/rtchat-47692/subscriptions/subscribe-sub").on(
-  "message",
-  async (message: Message) => {
-    const { provider, channel } = JSON.parse(message.data.toString());
-    if (await subscribe(provider, channel)) {
-      message.ack();
-    } else {
-      message.nack();
-    }
+async function unsubscribe(provider: string, channel: string) {
+  switch (provider) {
+    case "twitch":
+      TWITCH_CLIENT.part(channel);
+      return true;
   }
+  return false; // not handled by this agent.
+}
+
+async function onSubscribe(message: Message) {
+  const { provider, channel } = JSON.parse(message.data.toString());
+  if (await subscribe(provider, channel)) {
+    message.ack();
+  } else {
+    message.nack();
+  }
+}
+
+async function onUnsubscribe(message: Message) {
+  const { provider, channel } = JSON.parse(message.data.toString());
+  if (await unsubscribe(provider, channel)) {
+    message.ack();
+  } else {
+    message.nack();
+  }
+}
+
+const JOIN_TOPIC = new PubSub().topic("projects/rtchat-47692/topics/subscribe");
+
+const JOIN_SUBSCRIPTION = JOIN_TOPIC.subscription(
+  "projects/rtchat-47692/subscriptions/subscribe-sub"
 );
 
+JOIN_SUBSCRIPTION.on("message", onSubscribe);
+
+const LEAVE_TOPIC = new PubSub().topic(
+  "projects/rtchat-47692/topics/unsubscribe"
+);
+
+const AGENT_ID = uuidv4();
+const LEAVE_SUBSCRIPTION_ID = `projects/rtchat-47692/subscriptions/unsubscribe-${AGENT_ID}`;
+
+(async function () {
+  const [subscription] = await LEAVE_TOPIC.createSubscription(
+    LEAVE_SUBSCRIPTION_ID
+  );
+
+  subscription.on("message", onUnsubscribe);
+})();
+
 process.once("SIGTERM", async () => {
+  JOIN_SUBSCRIPTION.off("message", onSubscribe);
+
+  await LEAVE_TOPIC.subscription(LEAVE_SUBSCRIPTION_ID).delete();
+
   const twitch = TWITCH_CLIENT.getChannels().map((channel) =>
-    TOPIC.publish(Buffer.from(JSON.stringify({ provider: "twitch", channel })))
+    JOIN_TOPIC.publish(
+      Buffer.from(JSON.stringify({ provider: "twitch", channel }))
+    )
   );
 
   await Promise.all(twitch);
