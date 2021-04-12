5786c206446d5b0402304caf659e218d170ac8e4
Kevin Wang <kevmo314@gmail.com>
2021-04-11 22:39:14 -0400

    add send function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
-import * as functions from "firebase-functions";
 import { PubSub } from "@google-cloud/pubsub";
 import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import * as tmi from "tmi.js";
 
 admin.initializeApp();
 
@@ -45,3 +46,24 @@ export const cleanup = functions.pubsub
       await batch.commit();
     }
   });
+
+export const send = functions.https.onRequest(async (req, res) => {
+  const provider = req.body?.provider;
+  const channel = req.body?.channel;
+  const message = req.body?.message;
+  const identity = req.body?.identity;
+  if (!provider || !channel || !message || !identity) {
+    res.status(400).send("missing provider, channel, message, or identity");
+    return;
+  }
+
+  switch (provider) {
+    case "twitch":
+      const client = new tmi.Client({ channels: [channel], identity });
+      await client.connect();
+      res.status(200).send(await client.say(channel, message));
+      return;
+  }
+
+  res.status(400).send("invalid provider");
+});
