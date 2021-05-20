a4fca1ec18bd1af82435c74660f70f27ddb3b00b
Kevin Wang <kevmo314@gmail.com>
2021-05-20 02:28:59 -0400

    functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -42,6 +42,25 @@ export const unsubscribe = functions.https.onCall(async (data) => {
   return {};
 });
 
+async function getTwitchClient(uid: string, channel: string) {
+  const token = await getAccessToken(uid, "twitch");
+
+  const usernameDoc = await admin
+    .firestore()
+    .collection("profiles")
+    .doc(uid)
+    .get();
+
+  const username = usernameDoc.get("twitch")["login"];
+  const identity = { username, password: `oauth:${token}` };
+  const client = new tmi.Client({
+    channels: [channel],
+    identity,
+  });
+  await client.connect();
+  return client;
+}
+
 export const send = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
@@ -58,89 +77,79 @@ export const send = functions.https.onCall(async (data, context) => {
 
   switch (provider) {
     case "twitch":
-      const token = await getAccessToken(context.auth.uid, "twitch");
-
-      const usernameDoc = await admin
-        .firestore()
-        .collection("profiles")
-        .doc(context.auth.uid)
-        .get();
-
-      const username = usernameDoc.get("twitch")["login"];
-      const identity = { username, password: `oauth:${token}` };
-      const client = new tmi.Client({
-        channels: [channel],
-        identity,
-      });
-      await client.connect();
+      const client = await getTwitchClient(context.auth.uid, channel);
       return await client.say(channel, message);
   }
 
   throw new functions.https.HttpsError("invalid-argument", "invalid provider");
 });
 
-export const ban = functions.https.onCall(async (data) => {
+export const ban = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
   const channel = data?.channel;
   const username = data?.username;
   const reason = data?.reason;
-  const identity = data?.identity;
-  if (!provider || !channel || !username || !reason || !identity) {
+  if (!provider || !channel || !username || !reason) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username, reason, or identity"
+      "missing provider, channel, username, reason"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = new tmi.Client({ channels: [channel], identity });
-      await client.connect();
+      const client = await getTwitchClient(context.auth.uid, channel);
       return await client.ban(channel, username, reason);
   }
 
   throw new functions.https.HttpsError("invalid-argument", "invalid provider");
 });
 
-export const unban = functions.https.onCall(async (data) => {
+export const unban = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
   const channel = data?.channel;
   const username = data?.username;
-  const identity = data?.identity;
-  if (!provider || !channel || !username || !identity) {
+  if (!provider || !channel || !username) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username, or identity"
+      "missing provider, channel, username"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = new tmi.Client({ channels: [channel], identity });
-      await client.connect();
+      const client = await getTwitchClient(context.auth.uid, channel);
       return await client.unban(channel, username);
   }
 
   throw new functions.https.HttpsError("invalid-argument", "invalid provider");
 });
 
-export const timeout = functions.https.onCall(async (data) => {
+export const timeout = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
   const channel = data?.channel;
   const username = data?.username;
   const length = data?.length;
   const reason = data?.reason;
-  const identity = data?.identity;
-  if (!provider || !channel || !username || !length || !reason || !identity) {
+  if (!provider || !channel || !username || !length || !reason) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username, length, reason, or identity"
+      "missing provider, channel, username, length, reason"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = new tmi.Client({ channels: [channel], identity });
+      const client = await getTwitchClient(context.auth.uid, channel);
       await client.connect();
       return await client.timeout(channel, username, length, reason);
   }
@@ -148,21 +157,23 @@ export const timeout = functions.https.onCall(async (data) => {
   throw new functions.https.HttpsError("invalid-argument", "invalid provider");
 });
 
-export const deleteMessage = functions.https.onCall(async (data) => {
+export const deleteMessage = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
   const channel = data?.channel;
   const messageId = data?.messageId;
-  const identity = data?.identity;
-  if (!provider || !channel || !messageId || !identity) {
+  if (!provider || !channel || !messageId) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, messageId, or identity"
+      "missing provider, channel, messageId"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = new tmi.Client({ channels: [channel], identity });
+      const client = await getTwitchClient(context.auth.uid, channel);
       await client.connect();
       return await client.deletemessage(channel, messageId);
   }
