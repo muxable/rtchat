bdd739610f9883ad9f98993b56e6c2f5755de888
Kevin Wang <kevmo314@gmail.com>
2021-05-23 12:40:36 -0400

    sub

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import { PubSub } from "@google-cloud/pubsub";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import * as tmi from "tmi.js";
 import { app as authApp } from "./auth";
 import { getAccessToken } from "./oauth";
+import { getTwitchClient, getTwitchLogin } from "./twitch";
 
 admin.initializeApp({
   credential: admin.credential.cert(
@@ -12,72 +12,78 @@ admin.initializeApp({
   databaseURL: "https://rtchat-47692-default-rtdb.firebaseio.com",
 });
 
-export const subscribe = functions.https.onCall(async (data) => {
+export const subscribe = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
-  const channel = data?.channel;
-  if (!provider || !channel) {
+  const channelId = data?.channelId;
+  if (!provider || !channelId) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel"
+      "missing provider, channelId"
     );
   }
 
   const pubsub = new PubSub({ projectId: "rtchat-47692" });
 
-  return await pubsub
+  const channel = await getTwitchLogin(context.auth.uid, channelId);
+  if (!channel) {
+    return true;
+  }
+  // check if it's currently locked
+  const lock = await admin
+    .database()
+    .ref("locks")
+    .child(provider)
+    .child(channel)
+    .get();
+  if (lock.exists()) {
+    return true;
+  }
+  await pubsub
     .topic("projects/rtchat-47692/topics/subscribe")
     .publish(Buffer.from(JSON.stringify({ provider, channel })));
+  return true;
 });
 
-export const unsubscribe = functions.https.onCall(async (data) => {
+export const unsubscribe = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
   const provider = data?.provider;
-  const channel = data?.channel;
-  if (!provider || !channel) {
+  const channelId = data?.channelId;
+  if (!provider || !channelId) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel"
+      "missing provider, channelId"
     );
   }
 
   return {};
 });
 
-async function getTwitchClient(uid: string, channel: string) {
-  const token = await getAccessToken(uid, "twitch");
-
-  const usernameDoc = await admin
-    .firestore()
-    .collection("profiles")
-    .doc(uid)
-    .get();
-
-  const username = usernameDoc.get("twitch")["login"];
-  const identity = { username, password: `oauth:${token}` };
-  const client = new tmi.Client({
-    channels: [channel],
-    identity,
-  });
-  await client.connect();
-  return client;
-}
-
 export const send = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
   const provider = data?.provider;
-  const channel = data?.channel;
+  const channelId = data?.channelId;
   const message = data?.message;
-  if (!provider || !channel || !message) {
+  if (!provider || !channelId || !message) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, message"
+      "missing provider, channelId, message"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = await getTwitchClient(context.auth.uid, channel);
+      const channel = await getTwitchLogin(context.auth.uid, channelId);
+      if (!channel) {
+        return;
+      }
+      const client = await getTwitchClient(context.auth.uid, channelId);
       return await client.say(channel, message);
   }
 
@@ -89,19 +95,23 @@ export const ban = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
   const provider = data?.provider;
-  const channel = data?.channel;
+  const channelId = data?.channelId;
   const username = data?.username;
   const reason = data?.reason;
-  if (!provider || !channel || !username || !reason) {
+  if (!provider || !channelId || !username || !reason) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username, reason"
+      "missing provider, channelId, username, reason"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = await getTwitchClient(context.auth.uid, channel);
+      const channel = await getTwitchLogin(context.auth.uid, channelId);
+      if (!channel) {
+        return;
+      }
+      const client = await getTwitchClient(context.auth.uid, channelId);
       return await client.ban(channel, username, reason);
   }
 
@@ -113,18 +123,22 @@ export const unban = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
   const provider = data?.provider;
-  const channel = data?.channel;
+  const channelId = data?.channelId;
   const username = data?.username;
-  if (!provider || !channel || !username) {
+  if (!provider || !channelId || !username) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username"
+      "missing provider, channelId, username"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = await getTwitchClient(context.auth.uid, channel);
+      const channel = await getTwitchLogin(context.auth.uid, channelId);
+      if (!channel) {
+        return;
+      }
+      const client = await getTwitchClient(context.auth.uid, channelId);
       return await client.unban(channel, username);
   }
 
@@ -136,20 +150,24 @@ export const timeout = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
   const provider = data?.provider;
-  const channel = data?.channel;
+  const channelId = data?.channelId;
   const username = data?.username;
   const length = data?.length;
   const reason = data?.reason;
-  if (!provider || !channel || !username || !length || !reason) {
+  if (!provider || !channelId || !username || !length || !reason) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, username, length, reason"
+      "missing provider, channelId, username, length, reason"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = await getTwitchClient(context.auth.uid, channel);
+      const channel = await getTwitchLogin(context.auth.uid, channelId);
+      if (!channel) {
+        return;
+      }
+      const client = await getTwitchClient(context.auth.uid, channelId);
       await client.connect();
       return await client.timeout(channel, username, length, reason);
   }
@@ -162,20 +180,48 @@ export const deleteMessage = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
   const provider = data?.provider;
-  const channel = data?.channel;
+  const channelId = data?.channelId;
   const messageId = data?.messageId;
-  if (!provider || !channel || !messageId) {
+  if (!provider || !channelId || !messageId) {
     throw new functions.https.HttpsError(
       "invalid-argument",
-      "missing provider, channel, messageId"
+      "missing provider, channelId, messageId"
     );
   }
 
   switch (provider) {
     case "twitch":
-      const client = await getTwitchClient(context.auth.uid, channel);
+      const channel = await getTwitchLogin(context.auth.uid, channelId);
+      if (!channel) {
+        return;
+      }
+      const client = await getTwitchClient(context.auth.uid, channelId);
       await client.connect();
       return await client.deletemessage(channel, messageId);
+  }
+
+  throw new functions.https.HttpsError("invalid-argument", "invalid provider");
+});
+
+export const getViewerCount = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("permission-denied", "missing auth");
+  }
+  const provider = data?.provider;
+  const channelId = data?.channelId;
+  if (!provider || !channelId) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "missing provider, channelId, messageId"
+    );
+  }
+  const token = await getAccessToken(context.auth.uid, "twitch");
+
+  switch (provider) {
+    case "twitch":
+      const client = await getTwitchClient(context.auth.uid, channelId);
+      await client.connect();
+      return await client.deletemessage(channelId, messageId);
   }
 
   throw new functions.https.HttpsError("invalid-argument", "invalid provider");
---
+++ functions/src/twitch.ts
@@ -0,0 +1,41 @@
+import * as admin from "firebase-admin";
+import fetch from "node-fetch";
+import * as tmi from "tmi.js";
+import { getAccessToken, TWITCH_CLIENT_ID } from "./oauth";
+
+export async function getTwitchClient(uid: string, channel: string) {
+  const token = await getAccessToken(uid, "twitch");
+
+  const usernameDoc = await admin
+    .firestore()
+    .collection("profiles")
+    .doc(uid)
+    .get();
+
+  const username = usernameDoc.get("twitch")["login"];
+  const identity = { username, password: `oauth:${token}` };
+  const client = new tmi.Client({
+    channels: [channel],
+    identity,
+  });
+  await client.connect();
+  return client;
+}
+
+export async function getTwitchLogin(uid: string, id: string) {
+  const token = await getAccessToken(uid, "twitch");
+
+  const response = await fetch(`https://api.twitch.tv/helix/users?id=${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Client-Id": TWITCH_CLIENT_ID,
+    },
+  });
+
+  const json = await response.json();
+
+  if (json["data"].length == 0) {
+    return null;
+  }
+  return json["data"][0]["login"] as string;
+}
