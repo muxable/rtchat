b93e1058bf096a91b403b492fd082f480c065842
Kevin Wang <kevmo314@gmail.com>
2021-07-15 15:16:46 -0400

    rename field

--- functions/src/search.ts
+++ functions/src/search.ts
@@ -27,7 +27,7 @@ export const search = functions.https.onCall(async (data, context) => {
         provider: "twitch",
         channelId: channel.id,
         displayName: channel.display_name,
-        isLive: channel.is_live,
+        isOnline: channel.is_live,
         imageUrl: channel.thumbnail_url,
         title: channel.title,
       };
