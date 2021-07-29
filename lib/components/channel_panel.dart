6d6532dd99a1ff2589e70fcd99b5aa6bcc87a665
Kevin Wang <kevmo314@gmail.com>
2021-07-29 17:58:18 -0400

    fix: increase size of drag target (#85)

--- lib/components/channel_panel.dart
+++ lib/components/channel_panel.dart
@@ -51,94 +51,100 @@ class _ChannelPanelWidgetState extends State<ChannelPanelWidget> {
         color: Theme.of(context).primaryColor,
         child: Padding(
           padding: const EdgeInsets.only(left: 16),
-          child: Row(children: [
-            Consumer<LayoutModel>(builder: (context, layoutModel, child) {
+          child: GestureDetector(
+            onVerticalDragUpdate: (details) {
+              final layoutModel =
+                  Provider.of<LayoutModel>(context, listen: false);
               if (layoutModel.locked || widget.onResize == null) {
-                return Container();
+                return;
               }
-              return GestureDetector(
-                onVerticalDragUpdate: (details) =>
-                    widget.onResize!(details.delta.dy),
-                child: const Padding(
+              widget.onResize!(details.delta.dy);
+            },
+            child: Row(children: [
+              Consumer<LayoutModel>(builder: (context, layoutModel, child) {
+                if (layoutModel.locked) {
+                  return Container();
+                }
+                return const Padding(
                     padding: EdgeInsets.only(right: 16),
-                    child: Icon(Icons.drag_indicator)),
-              );
-            }),
-            Consumer<ChannelsModel>(builder: (context, channelsModel, child) {
-              if (channelsModel.subscribedChannels.isEmpty) {
-                return const Spacer();
-              }
-              final first = channelsModel.subscribedChannels.first;
-              return Expanded(
-                child: PopupMenuButton<_ChannelPickerValue>(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8),
-                            child: Image(
-                                height: 24,
-                                image: AssetImage(
-                                    'assets/providers/${first.provider}.png')),
-                          ),
-                          Expanded(
-                            child: Text("/${first.displayName}",
-                                softWrap: false, overflow: TextOverflow.fade),
-                          ),
-                        ]),
-                    onSelected: (value) async {
-                      if (value.isAdd) {
-                        // show the search dialog.
-                        await showDialog(
-                          context: context,
-                          builder: (context) =>
-                              ChannelSearchDialog(onSelect: (channel) {
-                            channelsModel.subscribedChannels = {channel};
-                            Navigator.pop(context);
+                    child: Icon(Icons.drag_indicator));
+              }),
+              Consumer<ChannelsModel>(builder: (context, channelsModel, child) {
+                if (channelsModel.subscribedChannels.isEmpty) {
+                  return const Spacer();
+                }
+                final first = channelsModel.subscribedChannels.first;
+                return Expanded(
+                  child: PopupMenuButton<_ChannelPickerValue>(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: Image(
+                                  height: 24,
+                                  image: AssetImage(
+                                      'assets/providers/${first.provider}.png')),
+                            ),
+                            Expanded(
+                              child: Text("/${first.displayName}",
+                                  softWrap: false, overflow: TextOverflow.fade),
+                            ),
+                          ]),
+                      onSelected: (value) async {
+                        if (value.isAdd) {
+                          // show the search dialog.
+                          await showDialog(
+                            context: context,
+                            builder: (context) =>
+                                ChannelSearchDialog(onSelect: (channel) {
+                              channelsModel.subscribedChannels = {channel};
+                              Navigator.pop(context);
+                            }),
+                          );
+                        } else {
+                          channelsModel.subscribedChannels = {value.channel!};
+                        }
+                      },
+                      itemBuilder: (context) {
+                        return [
+                          ...channelsModel.availableChannels.map((channel) {
+                            return PopupMenuItem(
+                                value: _ChannelPickerValue(channel: channel),
+                                child: ListTile(
+                                  title: Text(channel.displayName),
+                                ));
                           }),
-                        );
-                      } else {
-                        channelsModel.subscribedChannels = {value.channel!};
-                      }
-                    },
-                    itemBuilder: (context) {
-                      return [
-                        ...channelsModel.availableChannels.map((channel) {
-                          return PopupMenuItem(
-                              value: _ChannelPickerValue(channel: channel),
+                          const PopupMenuItem(
+                              value: _ChannelPickerValue(isAdd: true),
                               child: ListTile(
-                                title: Text(channel.displayName),
-                              ));
-                        }),
-                        const PopupMenuItem(
-                            value: _ChannelPickerValue(isAdd: true),
-                            child: ListTile(
-                                title: Text("Find a channel"),
-                                leading: Icon(Icons.add)))
-                      ];
-                    }),
-              );
-            }),
-            Consumer<ChannelsModel>(builder: (context, channelsModel, child) {
-              if (channelsModel.subscribedChannels.isEmpty) {
-                return Container();
-              }
-              final first = channelsModel.subscribedChannels.first;
-              return StatisticsBarWidget(
-                  provider: first.provider, channelId: first.channelId);
-            }),
-            Consumer<TtsModel>(builder: (context, ttsModel, child) {
-              return IconButton(
-                  icon: Icon(ttsModel.enabled
-                      ? Icons.record_voice_over
-                      : Icons.voice_over_off),
-                  tooltip: "Text to speech",
-                  onPressed: () {
-                    ttsModel.enabled = !ttsModel.enabled;
-                  });
-            }),
-          ]),
+                                  title: Text("Find a channel"),
+                                  leading: Icon(Icons.add)))
+                        ];
+                      }),
+                );
+              }),
+              Consumer<ChannelsModel>(builder: (context, channelsModel, child) {
+                if (channelsModel.subscribedChannels.isEmpty) {
+                  return Container();
+                }
+                final first = channelsModel.subscribedChannels.first;
+                return StatisticsBarWidget(
+                    provider: first.provider, channelId: first.channelId);
+              }),
+              Consumer<TtsModel>(builder: (context, ttsModel, child) {
+                return IconButton(
+                    icon: Icon(ttsModel.enabled
+                        ? Icons.record_voice_over
+                        : Icons.voice_over_off),
+                    tooltip: "Text to speech",
+                    onPressed: () {
+                      ttsModel.enabled = !ttsModel.enabled;
+                    });
+              }),
+            ]),
+          ),
         ),
       ),
     );
