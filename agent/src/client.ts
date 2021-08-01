6a06bda25b51f14792b18981059824fa997b31ef
Kevin Wang <kevmo314@gmail.com>
2021-08-01 19:31:43 -0400

    fix

--- agent/src/client.ts
+++ agent/src/client.ts
@@ -45,6 +45,7 @@ export function buildClient() {
       }
 
       await messages.doc(`twitch:-${messageId}`).set({
+        channel,
         channelId: original.get("channelId"),
         type: "messagedeleted",
         timestamp,
