42749de751e44b7b2e15112c0888f6f6c062b447
Kevin Wang <kevmo314@gmail.com>
2021-08-06 18:31:50 -0400

    fix uncaught exception

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -24,9 +24,15 @@ const agent = new Agent(AGENT_ID, {
   firebase: new FirebaseAdapter(admin.database(), admin.firestore()),
 });
 
-for (const signal of ["SIGINT", "SIGTERM", "uncaughtException"]) {
+for (const signal of ["SIGINT", "SIGTERM"]) {
   process.on(signal, async () => {
     await agent.disconnect();
     process.exit(0);
   });
 }
+
+process.on("uncaughtException", async (err) => {
+  console.error(err);
+  await agent.disconnect();
+  process.exit(1);
+});
