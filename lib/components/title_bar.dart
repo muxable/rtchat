324ae31610c9c206d3813438d54036243bdf516f
Kevin Wang <kevmo314@gmail.com>
2021-08-06 19:54:39 -0700

    chore: inline quick links widget into title bar (#116)
    * chore: inline quick links widget into title bar
    
    * remove unnecessary variable declaration

--- lib/components/title_bar.dart
+++ lib/components/title_bar.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:rtchat/components/quick_links_bar.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:provider/provider.dart';
 import 'package:rtchat/components/settings_button.dart';
+import 'package:rtchat/models/quick_links.dart';
+import 'package:rtchat/screens/settings/quick_links.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TitleBarWidget extends StatelessWidget {
-  const TitleBarWidget({Key? key}) : super(key: key);
+  final browser = ChromeSafariBrowser();
+
+  TitleBarWidget({Key? key}) : super(key: key);
+
+  void launchLink(QuickLinkSource source) async {
+    final isWebUrl =
+        source.url.scheme == 'http' || source.url.scheme == 'https';
+    if (isWebUrl) {
+      await browser.open(url: source.url);
+    } else {
+      await launch(source.url.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +27,7 @@ class TitleBarWidget extends StatelessWidget {
         height: 56,
         color: Theme.of(context).primaryColor,
         child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          // tabs
           const SizedBox(
             width: 168,
             child: TabBar(
@@ -20,7 +37,21 @@ class TitleBarWidget extends StatelessWidget {
               ],
             ),
           ),
-          QuickLinksBar(),
+          // quick links
+          Consumer<QuickLinksModel>(builder: (context, quickLinksModel, child) {
+            return Expanded(
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              reverse: true,
+              children: quickLinksModel.sources.reversed.map((source) {
+                return IconButton(
+                    icon: Icon(quickLinksIconsMap[source.icon] ?? Icons.link),
+                    tooltip: source.name,
+                    onPressed: () => launchLink(source));
+              }).toList(),
+            ));
+          }),
+          // settings button
           const SettingsButtonWidget()
         ]));
   }
