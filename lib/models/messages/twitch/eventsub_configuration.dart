be9db371a0141fb7465742c9d73c02635d24bf78
ChuangWei Ma <chungwwei223@gmail.com>
2021-08-20 10:29:10 -0400

    revise eventsub model

--- lib/models/messages/twitch/eventsub_configuration.dart
+++ lib/models/messages/twitch/eventsub_configuration.dart
@@ -1,111 +1,81 @@
 import 'package:flutter/material.dart';
 
+class FollowEventConfig {
+  final bool showEvent;
+  final bool isEventPinnable;
+  final int eventDuration;
+
+  FollowEventConfig(this.showEvent, this.isEventPinnable, this.eventDuration);
+
+  FollowEventConfig.fromJson(Map<String, dynamic> json)
+      : showEvent = json['showEvent'],
+        isEventPinnable = json['isEventPinnable'],
+        eventDuration = json['eventDuration'];
+
+  Map<String, dynamic> toJson() => {
+        "showEvent": showEvent,
+        "isEventPinnable": isEventPinnable,
+        "eventDuration": eventDuration,
+      };
+}
+
+class SubscriptionEventConfig {
+  final bool showEvent;
+  final bool isEventPinnable;
+  final int eventDuration;
+
+  SubscriptionEventConfig(
+      this.showEvent, this.isEventPinnable, this.eventDuration);
+
+  SubscriptionEventConfig.fromJson(Map<String, dynamic> json)
+      : showEvent = json['showEvent'],
+        isEventPinnable = json['isEventPinnable'],
+        eventDuration = json['eventDuration'];
+
+  Map<String, dynamic> toJson() => {
+        "showEvent": showEvent,
+        "isEventPinnable": isEventPinnable,
+        "eventDuration": eventDuration,
+      };
+}
+
+class CheerEventConfig {
+  final bool showEvent;
+  final bool isEventPinnable;
+  final int eventDuration;
+
+  CheerEventConfig(this.showEvent, this.isEventPinnable, this.eventDuration);
+
+  CheerEventConfig.fromJson(Map<String, dynamic> json)
+      : showEvent = json['showEvent'],
+        isEventPinnable = json['isEventPinnable'],
+        eventDuration = json['eventDuration'];
+
+  Map<String, dynamic> toJson() => {
+        "showEvent": showEvent,
+        "isEventPinnable": isEventPinnable,
+        "eventDuration": eventDuration,
+      };
+}
+
 class EventSubConfigurationModel extends ChangeNotifier {
-  bool showFollowEvent = true;
-  bool showCheerEvent = true;
-  bool showSubEvent = true;
-  bool showGiftedSubEvent = true;
-  bool showChannelPointEvent = true;
-  bool showHypeTrainEvent = true;
-  bool showPredictionEvent = true;
-  bool showPollEvent = true;
-
-  bool isFollowEventPinnable = false;
-  bool isCheerEventPinnable = false;
-  bool isSubEventPinnable = false;
-  bool isGiftedSubEventPinnable = false;
-  bool isChannelPointEventPinnable = false;
-  bool isHypeTrainEventPinnable = false;
-  bool isPredictionEventPinnable = false;
-  bool isPollEventPinnable = false;
-
-  double followEventPinDuration = 5;
-  double cheerEventPinDuration = 5;
-  double subEventPinDuration = 5;
-  double giftedSubEventPinDuration = 5;
-  double channelPointEventPinDuration = 5;
+  FollowEventConfig followEventConfig = FollowEventConfig(false, false, 5);
+  SubscriptionEventConfig subscriptionEventConfig =
+      SubscriptionEventConfig(false, false, 5);
+  CheerEventConfig cheerEventConfig = CheerEventConfig(true, true, 5);
+  // other event configs
+  // final HypeTrainEventConfig;
 
   EventSubConfigurationModel.fromJson(Map<String, dynamic> json) {
-    if (json['showFollowEvent'] != null) {
-      showFollowEvent = json['showFollowEvent'];
-    }
-    if (json['showCheerEvent'] != null) {
-      showCheerEvent = json['showCheerEvent'];
-    }
-    if (json['showSubEvent'] != null) {
-      showSubEvent = json['showSubEvent'];
-    }
-    if (json['showGiftedSubEvent'] != null) {
-      showGiftedSubEvent = json['showGiftedSubEvent'];
-    }
-    if (json['showChannelPointEvent'] != null) {
-      showChannelPointEvent = json['showChannelPointEvent'];
-    }
-    if (json['showHypeTrainEvent'] != null) {
-      showHypeTrainEvent = json['showHypeTrainEvent'];
-    }
-    if (json['showPredictionEvent'] != null) {
-      showPredictionEvent = json['showPredictionEvent'];
-    }
-    if (json['showPollEvent'] != null) {
-      showPollEvent = json['showPollEvent'];
-    }
-    if (json['isFollowEventPinnable'] != null) {
-      isFollowEventPinnable = json['isFollowEventPinnable'];
-    }
-    if (json['isCheerEventPinnable'] != null) {
-      isCheerEventPinnable = json['isCheerEventPinnable'];
-    }
-    if (json['isGiftedSubEventPinnable'] != null) {
-      isGiftedSubEventPinnable = json['isGiftedSubEventPinnable'];
-    }
-    if (json['isChannelPointEventPinnable'] != null) {
-      isChannelPointEventPinnable = json['isChannelPointEventPinnable'];
-    }
-    if (json['isPredictionEventPinnable'] != null) {
-      isPredictionEventPinnable = json['isPredictionEventPinnable'];
-    }
-    if (json['isPollEventPinnable'] != null) {
-      isPollEventPinnable = json['isPollEventPinnable'];
-    }
-    if (json['followEventPinDuration'] != null) {
-      followEventPinDuration = json['followEventPinDuration'];
-    }
-    if (json['cheerEventPinDuration'] != null) {
-      cheerEventPinDuration = json['cheerEventPinDuration'];
-    }
-    if (json['subEventPinDuration'] != null) {
-      subEventPinDuration = json['subEventPinDuration'];
-    }
-    if (json['giftedSubEventPinDuration'] != null) {
-      giftedSubEventPinDuration = json['giftedSubEventPinDuration'];
-    }
-    if (json['channelPointEventPinDuration'] != null) {
-      channelPointEventPinDuration = json['channelPointEventPinDuration'];
-    }
+    followEventConfig = FollowEventConfig.fromJson(json['followEventConfig']);
+    subscriptionEventConfig =
+        SubscriptionEventConfig.fromJson(json['subscriptionEventConfig']);
+    cheerEventConfig = CheerEventConfig.fromJson(json['cheerEventConfig']);
   }
 
   Map<String, dynamic> toJson() => {
-        'showFollowEvent': showFollowEvent,
-        'showCheerEvent': showCheerEvent,
-        'showSubEvent': showSubEvent,
-        'showGiftedSubEvent': showGiftedSubEvent,
-        'showChannelPointEvent': showChannelPointEvent,
-        'showHypeTrainEvent': showHypeTrainEvent,
-        'showPredictionEvent': showPredictionEvent,
-        'showPollEvent': showPollEvent,
-        'isFollowEventPinnable': isFollowEventPinnable,
-        'isCheerEventPinnable': isCheerEventPinnable,
-        'isSubEventPinnable': isSubEventPinnable,
-        'isGiftedSubEventPinnable': isGiftedSubEventPinnable,
-        'isChannelPointEventPinnable': isChannelPointEventPinnable,
-        'isHypeTrainEventPinnable': isHypeTrainEventPinnable,
-        'isPredictionEventPinnable': isPredictionEventPinnable,
-        'isPollEventPinnable': isPollEventPinnable,
-        'followEventPinDuration': followEventPinDuration,
-        'cheerEventPinDuration': cheerEventPinDuration,
-        'subEventPinDuration': subEventPinDuration,
-        'giftedSubEventPinDuration': giftedSubEventPinDuration,
-        'channelPointEventPinDuration': channelPointEventPinDuration,
+        "followEventConfig": followEventConfig,
+        'subscriptionEventConfig': subscriptionEventConfig,
+        'cheerEventConfig': cheerEventConfig
       };
 }
