e5a73086143c10bf523bb75276362a32f993ccdc
ChuangWei Ma <39935368+chungwwei@users.noreply.github.com>
2021-08-24 12:29:39 -0400

    feat: added event configuration selection screen and follow screen (#195)
    * fix: revise eventsub model
    
    * feat: added event config setting screen and follow event screen
    
    * fix: revised event models and added subtitles for switches
    
    * refactor: rename files and put config behind flag

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'package:rtchat/models/channels.dart';
 import 'package:rtchat/models/layout.dart';
 import 'package:rtchat/models/messages/tts_audio_handler.dart';
 import 'package:rtchat/models/messages/twitch/badge.dart';
+import 'package:rtchat/models/messages/twitch/eventsub_configuration.dart';
 import 'package:rtchat/models/quick_links.dart';
 import 'package:rtchat/models/style.dart';
 import 'package:rtchat/models/tts.dart';
@@ -27,6 +28,8 @@ import 'package:rtchat/screens/home.dart';
 import 'package:rtchat/screens/settings/activity_feed.dart';
 import 'package:rtchat/screens/settings/audio_sources.dart';
 import 'package:rtchat/screens/settings/backup.dart';
+import 'package:rtchat/screens/settings/events.dart';
+import 'package:rtchat/screens/settings/events/follow.dart';
 import 'package:rtchat/screens/settings/quick_links.dart';
 import 'package:rtchat/screens/settings/settings.dart';
 import 'package:rtchat/screens/settings/twitch/badges.dart';
@@ -204,6 +207,14 @@ class App extends StatelessWidget {
           });
           return model;
         }),
+        ChangeNotifierProvider(create: (context) {
+          final model = EventSubConfigurationModel.fromJson(
+              jsonDecode(prefs.getString("event_sub_configs") ?? "{}"));
+          model.addListener(() {
+            prefs.setString('event_sub_configs', jsonEncode(model.toJson()));
+          });
+          return model;
+        }),
       ],
       child: DefaultTabController(
         initialIndex: 0,
@@ -236,6 +247,8 @@ class App extends StatelessWidget {
             '/settings/audio-sources': (context) => const AudioSourcesScreen(),
             '/settings/quick-links': (context) => const QuickLinksScreen(),
             '/settings/backup': (context) => const BackupScreen(),
+            '/settings/events': (context) => const EventsScreen(),
+            '/settings/events/follow': (context) => const FollowEventScreen()
           },
         ),
       ),
--- lib/models/messages/twitch/eventsub_configuration.dart
+++ lib/models/messages/twitch/eventsub_configuration.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 class FollowEventConfig {
-  final bool showEvent;
-  final bool isEventPinnable;
-  final int eventDuration;
+  bool showEvent;
+  bool isEventPinnable;
+  Duration eventDuration;
 
   FollowEventConfig(this.showEvent, this.isEventPinnable, this.eventDuration);
 
   FollowEventConfig.fromJson(Map<String, dynamic> json)
       : showEvent = json['showEvent'],
         isEventPinnable = json['isEventPinnable'],
-        eventDuration = json['eventDuration'];
+        eventDuration = Duration(seconds: json['eventDuration'].toInt());
 
   Map<String, dynamic> toJson() => {
         "showEvent": showEvent,
         "isEventPinnable": isEventPinnable,
-        "eventDuration": eventDuration,
+        "eventDuration": eventDuration.inSeconds.toInt(),
       };
 }
 
 class SubscriptionEventConfig {
-  final bool showEvent;
-  final bool isEventPinnable;
-  final int eventDuration;
+  bool showEvent;
+  bool isEventPinnable;
+  Duration eventDuration;
 
   SubscriptionEventConfig(
       this.showEvent, this.isEventPinnable, this.eventDuration);
@@ -30,47 +30,70 @@ class SubscriptionEventConfig {
   SubscriptionEventConfig.fromJson(Map<String, dynamic> json)
       : showEvent = json['showEvent'],
         isEventPinnable = json['isEventPinnable'],
-        eventDuration = json['eventDuration'];
+        eventDuration = Duration(seconds: json['eventDuration'].toInt());
 
   Map<String, dynamic> toJson() => {
         "showEvent": showEvent,
         "isEventPinnable": isEventPinnable,
-        "eventDuration": eventDuration,
+        "eventDuration": eventDuration.inSeconds.toInt(),
       };
 }
 
 class CheerEventConfig {
-  final bool showEvent;
-  final bool isEventPinnable;
-  final int eventDuration;
+  bool showEvent;
+  bool isEventPinnable;
+  Duration eventDuration;
 
   CheerEventConfig(this.showEvent, this.isEventPinnable, this.eventDuration);
 
   CheerEventConfig.fromJson(Map<String, dynamic> json)
       : showEvent = json['showEvent'],
         isEventPinnable = json['isEventPinnable'],
-        eventDuration = json['eventDuration'];
+        eventDuration = Duration(seconds: json['eventDuration'].toInt());
 
   Map<String, dynamic> toJson() => {
         "showEvent": showEvent,
         "isEventPinnable": isEventPinnable,
-        "eventDuration": eventDuration,
+        "eventDuration": eventDuration.inSeconds.toInt(),
       };
 }
 
 class EventSubConfigurationModel extends ChangeNotifier {
-  FollowEventConfig followEventConfig = FollowEventConfig(false, false, 5);
+  FollowEventConfig followEventConfig =
+      FollowEventConfig(false, false, const Duration(seconds: 5));
   SubscriptionEventConfig subscriptionEventConfig =
-      SubscriptionEventConfig(false, false, 5);
-  CheerEventConfig cheerEventConfig = CheerEventConfig(true, true, 5);
-  // other event configs
+      SubscriptionEventConfig(false, false, const Duration(seconds: 5));
+  CheerEventConfig cheerEventConfig =
+      CheerEventConfig(true, true, const Duration(seconds: 5));
+  // other configs
   // final HypeTrainEventConfig;
 
+  setFollowEventDuration(Duration duration) {
+    followEventConfig.eventDuration = duration;
+    notifyListeners();
+  }
+
+  setFollowEventShowable(bool value) {
+    followEventConfig.showEvent = value;
+    notifyListeners();
+  }
+
+  setFollowEventPinnable(bool value) {
+    followEventConfig.isEventPinnable = value;
+    notifyListeners();
+  }
+
   EventSubConfigurationModel.fromJson(Map<String, dynamic> json) {
-    followEventConfig = FollowEventConfig.fromJson(json['followEventConfig']);
-    subscriptionEventConfig =
-        SubscriptionEventConfig.fromJson(json['subscriptionEventConfig']);
-    cheerEventConfig = CheerEventConfig.fromJson(json['cheerEventConfig']);
+    if (json['followEventConfig'] != null) {
+      followEventConfig = FollowEventConfig.fromJson(json['followEventConfig']);
+    }
+    if (json['subscriptionEventConfig'] != null) {
+      subscriptionEventConfig =
+          SubscriptionEventConfig.fromJson(json['subscriptionEventConfig']);
+    }
+    if (json['cheerEventConfig'] != null) {
+      cheerEventConfig = CheerEventConfig.fromJson(json['cheerEventConfig']);
+    }
   }
 
   Map<String, dynamic> toJson() => {
