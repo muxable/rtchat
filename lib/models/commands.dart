b89e8664216a145dcbd7aa078f8e9098beee27e5
Ryan <ryansngrq@gmail.com>
2021-09-08 22:42:58 +0800

    added Command class

--- lib/models/commands.dart
+++ lib/models/commands.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+class Command {
+  String _command;
+  DateTime _timeLastUsed;
+
+  Command(this._command, this._timeLastUsed);
+
+  String get command => _command;
+
+  DateTime get timeLastUsed => _timeLastUsed;
+
+  @override
+  bool operator ==(other) => other is Command && other.command == _command;
+
+  @override
+  String toString() => '$_command:$_timeLastUsed';
+}
+
 class CommandsModel extends ChangeNotifier {
-  List<String> _commands = [];
+  List<Command> _commandList = [];
   static const _maxNumberOfCommands = 10;
 
-  List<String> get commands => _commands;
+  List<Command> get commandList => _commandList;
 
   void clear() {
-    _commands.clear();
+    _commandList.clear();
     notifyListeners();
   }
 
-  void addCommand(String command) {
-    if (_commands.contains(command)) {
-      _commands.remove(command);
+  void addCommand(Command command) {
+    if (_commandList.contains(command)) {
+      _commandList.remove(command);
     }
-    if (_commands.length >= _maxNumberOfCommands) {
-      _commands.removeLast();
+    if (_commandList.length >= _maxNumberOfCommands) {
+      _commandList.removeLast();
     }
-    _commands.insert(0, command);
+    _commandList.insert(0, command);
     notifyListeners();
   }
 
   CommandsModel.fromJson(Map<String, dynamic> json) {
     if (json['commands'] != null) {
-      _commands = List<String>.from(json['commands']);
+      _commandList = List<Command>.from(json['commands']);
     }
   }
 
-  Map<String, dynamic> toJson() => {'commands': _commands};
+  Map<String, dynamic> toJson() => {'commands': _commandList};
 }
