f9311c18e800137151aa63c2889d5b02f5d56365
Kevin Wang <kevmo314@gmail.com>
2021-09-13 12:39:53 -0400

    fix method channel

--- test/models/tts_test.dart
+++ test/models/tts_test.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rtchat/models/messages/tts_audio_handler.dart';
 import 'package:rtchat/models/tts.dart';
 
 void main() {
-  test("TtsModel json roundtrip", () {
+  testWidgets("TtsModel json roundtrip", (tester) async {
+    tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
+        const MethodChannel('flutter_tts'), (methodCall) {});
     final handler = TtsAudioHandler();
     final model = TtsModel.fromJson(handler, {});
     final want = model.toJson();
