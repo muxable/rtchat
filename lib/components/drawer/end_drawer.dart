de49a8ab893664ca1e692b102aff31d7baf999ef
ChuangWei Ma <39935368+chungwwei@users.noreply.github.com>
2022-03-23 22:03:10 -0400

    feat: list viewers in a drawer (#308)
    * feat: list viewers
    
    * fix: lowercase channel name
    
    * chore: remove string interpolation bracket
    
    * fix: darkmode awareness and discard headers if empty list
    
    * refactor: used guarded clause
    
    * chore: remove commented out code

---
+++ lib/components/drawer/end_drawer.dart
@@ -0,0 +1,136 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:rtchat/components/drawer/sliver_title.dart';
+import 'package:rtchat/models/channels.dart';
+
+class LeftDrawerWidget extends StatefulWidget {
+  const LeftDrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LeftDrawerWidget> createState() => LeftDrawerWidgetState();
+}
+
+class LeftDrawerWidgetState extends State<LeftDrawerWidget> {
+  Future<dynamic>? viewersFuture;
+
+  _viewersFuture(String channel) async {
+    Uri uri = Uri.parse('https://tmi.twitch.tv/group/user/$channel/chatters');
+    final res = await http.get(uri);
+    if (res.statusCode != 200) {
+      return {"broadcaster": [], "moderators": [], "vips": [], "viewers": []};
+    } else {
+      final jsonBody = jsonDecode(res.body);
+      List<String> broadcasterList =
+          List<String>.from(jsonBody['chatters']['broadcaster']);
+      List<String> moderatorList =
+          List<String>.from(jsonBody['chatters']['moderators']);
+      List<String> vipList = List<String>.from(jsonBody['chatters']['vips']);
+      List<String> viewerList =
+          List<String>.from(jsonBody['chatters']['viewers']);
+
+      return {
+        "broadcaster": broadcasterList,
+        "moderators": moderatorList,
+        "vips": vipList,
+        "viewers": viewerList
+      };
+    }
+  }
+
+  @override
+  void initState() {
+    final channelsModel = Provider.of<ChannelsModel>(context, listen: false);
+    final channel = channelsModel.subscribedChannels.first;
+    viewersFuture = _viewersFuture(channel.displayName.toLowerCase());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+    bool isDarkMode = brightness == Brightness.dark;
+    return Container(
+      color: isDarkMode
+          ? Colors.black.withOpacity(0.85)
+          : Colors.white.withOpacity(0.9),
+      child: FutureBuilder(
+        future: viewersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, List<String>> data =
+                snapshot.data as Map<String, List<String>>;
+            final broadcasterList = data['broadcaster'];
+            final moderatorList = data['moderators'];
+            final vipList = data['vips'];
+            final viewerList = data['viewers'];
+            return CustomScrollView(
+              slivers: [
+                // const SliverSearchBarWidget(),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    const [
+                      SizedBox(
+                        height: 64,
+                      )
+                    ],
+                  ),
+                ),
+                const SliverTitleWidget(title: "Broadcaster"),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) => Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(broadcasterList![index]),
+                    ),
+                    childCount: broadcasterList!.length,
+                  ),
+                ),
+                if (moderatorList!.isNotEmpty) ...[
+                  const SliverTitleWidget(title: "Moderators")
+                ],
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) => Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(moderatorList[index]),
+                    ),
+                    childCount: moderatorList.length,
+                  ),
+                ),
+                if (vipList!.isNotEmpty) ...[
+                  const SliverTitleWidget(title: "Community VIPs")
+                ],
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) => Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(vipList[index]),
+                    ),
+                    childCount: vipList.length,
+                  ),
+                ),
+                if (viewerList!.isNotEmpty) ...[
+                  const SliverTitleWidget(title: "Viewers")
+                ],
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) => Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(viewerList[index]),
+                    ),
+                    childCount: viewerList.length,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
