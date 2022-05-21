289f57a511ba3abec767b8074fe929ae5e1dbf47
Kevin Wang <kevmo314@gmail.com>
2022-05-21 15:15:20 -0700

    fix: prediction event parsing (#437)

--- lib/models/messages/twitch/prediction_event.dart
+++ lib/models/messages/twitch/prediction_event.dart
@@ -37,13 +37,13 @@ class TwitchPredictionEventModel extends MessageModel {
         title: data['event']['title'],
         status: "in_progress",
         endTime: DateTime.parse(data['event']['locks_at']),
-        outcomes: List.from(data['event']['outcomes'].values.map((outcome) {
+        outcomes: data['event']['outcomes'].map((outcome) {
           return TwitchPredictionOutcomeModel(
               outcome['id'],
               outcome['channel_points'] ?? 0,
               outcome['color'],
               outcome['title']);
-        })));
+        }).toList());
   }
 
   static TwitchPredictionEventModel fromEndEvent(Map<String, dynamic> data) {
