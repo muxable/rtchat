c815ec2711ebdb24b893eb78e9a661e42c43ab4d
Kevin Wang <kevmo314@gmail.com>
2022-05-23 20:15:38 -0400

    feat: add toggle for light/dark mode

--- lib/models/layout.dart
+++ lib/models/layout.dart
@@ -1,6 +1,7 @@
 import 'dart:core';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 enum PreferredOrientation {
@@ -34,6 +35,31 @@ extension _OrientationPreferenceJson on PreferredOrientation {
   }
 }
 
+extension _ThemeModeJson on ThemeMode {
+  int toJson() {
+    switch (this) {
+      case ThemeMode.light:
+        return 0;
+      case ThemeMode.dark:
+        return 1;
+      case ThemeMode.system:
+        return 2;
+    }
+  }
+
+  static ThemeMode fromJson(dynamic json) {
+    switch (json) {
+      case 0:
+        return ThemeMode.light;
+      case 1:
+        return ThemeMode.dark;
+      case 2:
+        return ThemeMode.system;
+    }
+    return ThemeMode.system;
+  }
+}
+
 class LayoutModel extends ChangeNotifier {
   double _panelHeight = 300.0;
   double _panelWidth = 300.0;
@@ -44,6 +70,7 @@ class LayoutModel extends ChangeNotifier {
   PreferredOrientation _orientationPreference = PreferredOrientation.system;
   bool _isShowNotifications = false;
   bool _isShowPreview = false;
+  ThemeMode _themeMode = ThemeMode.system;
 
   void updatePanelHeight({required double dy}) {
     _panelHeight += dy;
@@ -102,6 +129,13 @@ class LayoutModel extends ChangeNotifier {
   set preferredOrientation(PreferredOrientation value) {
     _orientationPreference = value;
     _bindOrientationPreference();
+    notifyListeners();
+  }
+
+  ThemeMode get themeMode => _themeMode;
+
+  set themeMode(ThemeMode value) {
+    _themeMode = value;
     notifyListeners();
   }
 
@@ -168,6 +202,9 @@ class LayoutModel extends ChangeNotifier {
     if (json['isShowPreview'] != null) {
       _isShowPreview = json['isShowPreview'];
     }
+    if (json['themeMode'] != null) {
+      _themeMode = _ThemeModeJson.fromJson(json['themeMode']);
+    }
   }
 
   Map<String, dynamic> toJson() => {
@@ -179,5 +216,6 @@ class LayoutModel extends ChangeNotifier {
         "orientationPreference": _orientationPreference.toJson(),
         "isShowNotifications": _isShowNotifications,
         "isShowPreview": _isShowPreview,
+        "themeMode": _themeMode.toJson(),
       };
 }
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -125,6 +125,50 @@ class _SettingsScreenState extends State<SettingsScreen> {
                       ],
                     ),
                   ])),
+          const SizedBox(height: 8),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("App theme",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const SizedBox(height: 8),
+                    ToggleButtons(
+                      constraints: BoxConstraints(
+                          minWidth:
+                              (MediaQuery.of(context).size.width - 36) / 3),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                      onPressed: (index) {
+                        switch (index) {
+                          case 0:
+                            layoutModel.themeMode = ThemeMode.system;
+                            break;
+                          case 1:
+                            layoutModel.themeMode = ThemeMode.light;
+                            break;
+                          case 2:
+                            layoutModel.themeMode = ThemeMode.dark;
+                            break;
+                        }
+                      },
+                      isSelected: [
+                        layoutModel.themeMode == ThemeMode.system,
+                        layoutModel.themeMode == ThemeMode.light,
+                        layoutModel.themeMode == ThemeMode.dark,
+                      ],
+                      selectedColor: Theme.of(context).colorScheme.secondary,
+                      children: [
+                        _iconWithText(Icons.auto_mode, "System"),
+                        _iconWithText(Icons.light_mode, "Light mode"),
+                        _iconWithText(Icons.dark_mode, "Dark mode"),
+                      ],
+                    ),
+                  ])),
           SwitchListTile.adaptive(
             title: const Text('Show viewer and follower count'),
             value: layoutModel.isStatsVisible,
