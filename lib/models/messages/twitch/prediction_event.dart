ca5481b38ee2028e02bdf6a0e49491cb4e97720f
Kevin Wang <kevmo314@gmail.com>
2022-06-03 14:27:03 -0400

    fix

--- lib/models/messages/twitch/prediction_event.dart
+++ lib/models/messages/twitch/prediction_event.dart
@@ -12,13 +12,6 @@ class TwitchPredictionOutcomeModel {
   MaterialColor get widgetColor => color == "pink" ? Colors.pink : Colors.blue;
 }
 
-Iterable<dynamic> _coerce(dynamic value) {
-  if (value is Map) {
-    return value.values;
-  }
-  return value;
-}
-
 class TwitchPredictionEventModel extends MessageModel {
   final String title;
   final String? status;
@@ -44,7 +37,7 @@ class TwitchPredictionEventModel extends MessageModel {
         title: data['event']['title'],
         status: "in_progress",
         endTime: DateTime.parse(data['event']['locks_at']),
-        outcomes: _coerce(data['event']['outcomes']).map((outcome) {
+        outcomes: data['event']['outcomes'].map((outcome) {
           return TwitchPredictionOutcomeModel(
               outcome['id'],
               outcome['channel_points'] ?? 0,
@@ -61,7 +54,7 @@ class TwitchPredictionEventModel extends MessageModel {
         status: data['event']['status'],
         winningOutcomeId: data['event']['winning_outcome_id'],
         endTime: DateTime.parse(data['event']['ended_at']),
-        outcomes: _coerce(data['event']['outcomes']).map((outcome) {
+        outcomes: data['event']['outcomes'].map((outcome) {
           return TwitchPredictionOutcomeModel(outcome['id'],
               outcome['channel_points'], outcome['color'], outcome['title']);
         }).toList());
