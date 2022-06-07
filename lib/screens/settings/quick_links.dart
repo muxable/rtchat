d7d680a7a9e1ce6acd74f209d098e7650e975d2a
Kevin Wang <kevmo314@gmail.com>
2022-06-07 19:15:34 +0000

    tweak quicklinks ui

--- lib/screens/settings/quick_links.dart
+++ lib/screens/settings/quick_links.dart
@@ -37,6 +37,24 @@ class _QuickLinksScreenState extends State<QuickLinksScreen> {
   final _textEditingController = TextEditingController();
   final _labelEditingController = TextEditingController();
   String _activeIcon = "view_list";
+  String _url = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController.addListener(() {
+      setState(() => _url = _textEditingController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    _labelEditingController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,15 +89,7 @@ class _QuickLinksScreenState extends State<QuickLinksScreen> {
           child: Form(
               key: _formKey,
               child: Column(children: [
-                TextFormField(
-                  controller: _labelEditingController,
-                  decoration: const InputDecoration(
-                    labelText: "Label",
-                    hintText: "Label",
-                    prefixIcon: Icon(Icons.abc_outlined),
-                  ),
-                ),
-                Row(children: [
+                Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
                   PopupMenuButton<String>(
                     icon: Icon(quickLinksIconsMap[_activeIcon] ?? Icons.link),
                     onSelected: (result) {
@@ -97,46 +107,61 @@ class _QuickLinksScreenState extends State<QuickLinksScreen> {
                     },
                   ),
                   Expanded(
-                    child: TextFormField(
-                        controller: _textEditingController,
-                        decoration: InputDecoration(
-                            hintText: "URL",
-                            suffixIcon: IconButton(
-                                icon: const Icon(Icons.qr_code_scanner),
-                                onPressed: () {
-                                  showModalBottomSheet<void>(
-                                      context: context,
-                                      builder: (context) {
-                                        return MobileScanner(
-                                            allowDuplicates: false,
-                                            onDetect: (barcode, args) {
-                                              final code = barcode.rawValue;
-                                              if (code != null) {
-                                                _textEditingController.text =
-                                                    code;
-                                              }
-                                              Navigator.of(context).pop();
-                                            });
-                                      });
-                                })),
-                        validator: (value) {
-                          if (value == null ||
-                              value.isEmpty ||
-                              Uri.tryParse(value) == null) {
-                            return "This doesn't look like a valid URL.";
-                          }
-                          final quickLinksModel = Provider.of<QuickLinksModel>(
-                              context,
-                              listen: false);
-                          if (quickLinksModel.sources.any((s) =>
-                              s.url.toString() == Uri.encodeFull(value))) {
-                            return "This link already exists";
-                          }
-                          return null;
-                        },
-                        keyboardType: TextInputType.url,
-                        textInputAction: TextInputAction.done,
-                        onEditingComplete: addLink),
+                    child: Column(children: [
+                      _url.isNotEmpty
+                          ? FutureBuilder<String>(
+                              future: retrieveName(_url),
+                              builder: (context, snapshot) {
+                                return TextFormField(
+                                  controller: _labelEditingController,
+                                  decoration: InputDecoration(
+                                    hintText: snapshot.data ?? "Label",
+                                  ),
+                                );
+                              },
+                            )
+                          : Container(),
+                      TextFormField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              hintText: "URL",
+                              suffixIcon: IconButton(
+                                  icon: const Icon(Icons.qr_code_scanner),
+                                  onPressed: () {
+                                    showModalBottomSheet<void>(
+                                        context: context,
+                                        builder: (context) {
+                                          return MobileScanner(
+                                              allowDuplicates: false,
+                                              onDetect: (barcode, args) {
+                                                final code = barcode.rawValue;
+                                                if (code != null) {
+                                                  _textEditingController.text =
+                                                      code;
+                                                }
+                                                Navigator.of(context).pop();
+                                              });
+                                        });
+                                  })),
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                Uri.tryParse(value) == null) {
+                              return "This doesn't look like a valid URL.";
+                            }
+                            final quickLinksModel =
+                                Provider.of<QuickLinksModel>(context,
+                                    listen: false);
+                            if (quickLinksModel.sources.any((s) =>
+                                s.url.toString() == Uri.encodeFull(value))) {
+                              return "This link already exists";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                          onEditingComplete: addLink),
+                    ]),
                   ),
                   IconButton(
                     icon: const Icon(Icons.add),
@@ -149,22 +174,28 @@ class _QuickLinksScreenState extends State<QuickLinksScreen> {
     );
   }
 
-  Future<String> retrieveName(QuickLinkSource link) async {
-    final metadata = await MetadataFetch.extract(link.url.toString());
-    return metadata?.title ?? link.url.toString();
+  Future<String> retrieveName(String url) async {
+    try {
+      final metadata = await MetadataFetch.extract(url);
+      return metadata?.title ?? url;
+    } catch (e) {
+      return url;
+    }
   }
 
-  void addLink() {
-    if (_formKey.currentState!.validate()) {
-      if (_labelEditingController.text.isEmpty) {
-        _labelEditingController.text = "My Link";
-      }
-      Provider.of<QuickLinksModel>(context, listen: false).addSource(
-          QuickLinkSource(_activeIcon, Uri.parse(_textEditingController.text),
-              _labelEditingController.text));
-      _textEditingController.clear();
-      _labelEditingController.clear();
-      FocusScope.of(context).unfocus();
+  void addLink() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
     }
+    final model = Provider.of<QuickLinksModel>(context, listen: false);
+    final focus = FocusScope.of(context);
+    final label = _labelEditingController.text.isEmpty
+        ? await retrieveName(_url)
+        : _labelEditingController.text;
+    model.addSource(QuickLinkSource(
+        _activeIcon, Uri.parse(_textEditingController.text), label));
+    _textEditingController.clear();
+    _labelEditingController.clear();
+    focus.unfocus();
   }
 }
