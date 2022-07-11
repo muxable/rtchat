21f9634a1ad0bcdeda71ee24291bf814db7f24bd
Kevin Wang <kevmo314@gmail.com>
2022-07-11 18:55:11 -0400

    add sentinel

--- lib/components/connection_status.dart
+++ lib/components/connection_status.dart
@@ -2,12 +2,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:rtchat/components/chat_panel.dart';
 
-Stream<DateTime?> lastDisconnectTime() {
-  return FirebaseDatabase.instance.ref('.info/connected').onValue.map((event) {
-    return event.snapshot.value == false ? DateTime.now() : null;
-  });
-}
-
 class ConnectionStatusWidget extends StatelessWidget {
   final _delay = const Duration(seconds: 5);
 
@@ -15,27 +9,40 @@ class ConnectionStatusWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DateTime?>(
-        stream: lastDisconnectTime(),
-        builder: (context, snapshot) {
-          final data = snapshot.data;
-          if (data == null) {
-            return Container();
-          }
-          return RebuildableWidget(
-              rebuildAt: {data.add(_delay)},
-              builder: (context) {
-                if (DateTime.now().difference(data) < _delay) {
-                  return Container();
-                }
-                return Container(
-                  width: double.infinity,
-                  height: 48,
-                  alignment: Alignment.center,
-                  color: Colors.red,
-                  child: const Text("Reconnecting..."),
-                );
-              });
-        });
+    return Stack(
+      children: [
+        // add a sentinel listener so Firebase connects to the db.
+        StreamBuilder(
+            stream: FirebaseDatabase.instance.ref('sentinel').onValue,
+            builder: (context, snapshot) => Container()),
+        StreamBuilder<DateTime?>(
+            stream: FirebaseDatabase.instance
+                .ref('.info/connected')
+                .onValue
+                .map((event) {
+              return event.snapshot.value == false ? DateTime.now() : null;
+            }),
+            builder: (context, snapshot) {
+              final data = snapshot.data;
+              if (data == null) {
+                return Container();
+              }
+              return RebuildableWidget(
+                  rebuildAt: {data.add(_delay)},
+                  builder: (context) {
+                    if (DateTime.now().difference(data) < _delay) {
+                      return Container();
+                    }
+                    return Container(
+                      width: double.infinity,
+                      height: 48,
+                      alignment: Alignment.center,
+                      color: Colors.red,
+                      child: const Text("Reconnecting..."),
+                    );
+                  });
+            }),
+      ],
+    );
   }
 }
