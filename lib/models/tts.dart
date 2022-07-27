1193383efdd3faf3180970ecf3f52e2deccdf134
Kevin Wang <kevmo314@gmail.com>
2022-07-27 10:37:34 -0400

    fix: remove dashes (#571)

--- lib/models/tts.dart
+++ lib/models/tts.dart
@@ -110,7 +110,7 @@ class TtsModel extends ChangeNotifier {
     }
     say(
         SystemMessageModel(
-            text: "Text-to-speech ${value ? "enabled" : "disabled"}"),
+            text: "Text to speech ${value ? "enabled" : "disabled"}"),
         force: true);
     notifyListeners();
   }
