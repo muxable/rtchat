be2783892177d3b67bf2a267ed257bc671262971
Kevin Wang <kevmo314@gmail.com>
2022-07-27 18:03:50 -0400

    feat: use random twitch emote for emote picker icon (#573)
    * feat: use random twitch emote for emote picker icon
    
    * increase resolution

--- lib/components/message_input.dart
+++ lib/components/message_input.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:provider/provider.dart';
 import 'package:rtchat/components/emote_picker.dart';
+import 'package:rtchat/components/image/resilient_network_image.dart';
 import 'package:rtchat/models/adapters/actions.dart';
 import 'package:rtchat/models/channels.dart';
 import 'package:rtchat/models/chat_mode.dart';
@@ -18,17 +19,28 @@ class MessageInputWidget extends StatefulWidget {
   State<MessageInputWidget> createState() => _MessageInputWidgetState();
 }
 
+final _emotes = [
+  "https://static-cdn.jtvnw.net/emoticons/v2/425618/default/light/2.0",
+  "https://static-cdn.jtvnw.net/emoticons/v2/112291/default/light/2.0",
+  "https://static-cdn.jtvnw.net/emoticons/v2/81274/default/light/2.0",
+  "https://static-cdn.jtvnw.net/emoticons/v2/28087/default/light/2.0",
+  "https://static-cdn.jtvnw.net/emoticons/v2/305954156/default/light/2.0",
+];
+
+const _greyscale = ColorFilter.matrix([
+  0.2126, 0.7152, 0.0722, 0, 0, // red
+  0.2126, 0.7152, 0.0722, 0, 0, // green
+  0.2126, 0.7152, 0.0722, 0, 0, // blue
+  0, 0, 0, 1, 0, // alpha
+]);
+
 class _MessageInputWidgetState extends State<MessageInputWidget> {
   final _textEditingController = TextEditingController();
   final _chatInputFocusNode = FocusNode();
   var _isEmotePickerVisible = false;
+  var _emoteIndex = Random().nextInt(_emotes.length);
 
   OverlayEntry? entry;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   bool startsWithPossibleCommands(String text) {
     if (text == "" || text.isEmpty) {
@@ -203,13 +215,22 @@ class _MessageInputWidgetState extends State<MessageInputWidget> {
                                 _chatInputFocusNode.requestFocus();
                               } else {
                                 _chatInputFocusNode.unfocus();
-                                setState(() => _isEmotePickerVisible = true);
+                                setState(() {
+                                  _isEmotePickerVisible = true;
+                                  _emoteIndex =
+                                      Random().nextInt(_emotes.length);
+                                });
                               }
                             },
                             splashRadius: 24,
-                            icon: Icon(_isEmotePickerVisible
-                                ? Icons.keyboard_rounded
-                                : Icons.tag_faces_rounded)),
+                            icon: _isEmotePickerVisible
+                                ? const Icon(Icons.keyboard_rounded)
+                                : ColorFiltered(
+                                    colorFilter: _greyscale,
+                                    child: Image(
+                                      image: ResilientNetworkImage(
+                                          Uri.parse(_emotes[_emoteIndex])),
+                                    ))),
                       ),
                       suffixIcon: Material(
                         color: Theme.of(context).inputDecorationTheme.fillColor,
