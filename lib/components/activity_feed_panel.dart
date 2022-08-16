5d712d52468eb79f0350ec18f95b558a4ff18683
Kevin Wang <kevmo314@gmail.com>
2022-08-16 12:01:53 -0400

    chore: remove flutter_inappwebview (#608)

--- lib/components/activity_feed_panel.dart
+++ lib/components/activity_feed_panel.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:provider/provider.dart';
 import 'package:rtchat/models/activity_feed.dart';
 import 'package:rtchat/models/user.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ActivityFeedPanelWidget extends StatefulWidget {
   const ActivityFeedPanelWidget({Key? key}) : super(key: key);
@@ -16,7 +16,7 @@ class ActivityFeedPanelWidget extends StatefulWidget {
 
 class _ActivityFeedPanelWidgetState extends State<ActivityFeedPanelWidget> {
   ActivityFeedModel? _activityFeedModel;
-  InAppWebViewController? _activityFeedController;
+  WebViewController? _activityFeedController;
 
   @override
   void initState() {
@@ -35,45 +35,38 @@ class _ActivityFeedPanelWidgetState extends State<ActivityFeedPanelWidget> {
 
   void synchronizeActivityFeedUrl() async {
     final uri = getUri();
-    if (uri != null && uri.toString().isNotEmpty) {
-      await _activityFeedController?.loadUrl(urlRequest: URLRequest(url: uri));
+    if (uri != null && uri.isNotEmpty) {
+      await _activityFeedController?.loadUrl(uri);
     }
   }
 
-  Uri? getUri() {
+  String? getUri() {
     final activityFeedModel =
         Provider.of<ActivityFeedModel>(context, listen: false);
     final userModel = Provider.of<UserModel>(context, listen: false);
     final channel = userModel.userChannel;
     if (activityFeedModel.isCustom) {
-      return Uri.tryParse(activityFeedModel.customUrl);
+      return activityFeedModel.customUrl;
     } else if (channel == null) {
       return null;
     }
     switch (channel.provider) {
       case "twitch":
-        return Uri.tryParse(
-            "https://dashboard.twitch.tv/popout/u/${channel.displayName}/stream-manager/activity-feed");
+        return "https://dashboard.twitch.tv/popout/u/${channel.displayName}/stream-manager/activity-feed";
     }
     return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    return InAppWebView(
-      initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-              javaScriptEnabled: true,
-              mediaPlaybackRequiresUserGesture: false,
-              transparentBackground: true),
-          android: AndroidInAppWebViewOptions(
-            useHybridComposition: true,
-          ),
-          ios: IOSInAppWebViewOptions(
-            allowsInlineMediaPlayback: true,
-          )),
-      initialUrlRequest: URLRequest(url: getUri()),
-      onWebViewCreated: (controller) => _activityFeedController = controller,
+    return WebView(
+      initialUrl: getUri(),
+      javascriptMode: JavascriptMode.unrestricted,
+      initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+      allowsInlineMediaPlayback: true,
+      zoomEnabled: false,
+      onWebViewCreated: (controller) =>
+          setState(() => _activityFeedController = controller),
       gestureRecognizers: {
         Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer()),
       },
