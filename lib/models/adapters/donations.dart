6a6a5f80e29436cb72f900aa3025c0fc7ea077f8
Kevin Wang <kevmo314@gmail.com>
2022-08-19 12:03:33 -0400

    feat: add donations adapter (#628)

---
+++ lib/models/adapters/donations.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+
+class StreamlabsConfig {
+  final String? currency;
+
+  StreamlabsConfig({this.currency});
+}
+
+class DonationsAdapter {
+  final FirebaseFirestore db;
+  final FirebaseFunctions functions;
+
+  DonationsAdapter._({required this.db, required this.functions});
+
+  static DonationsAdapter get instance =>
+      _instance ??= DonationsAdapter._(
+      db: FirebaseFirestore.instance, functions: FirebaseFunctions.instance);
+  static DonationsAdapter? _instance;
+
+  Stream<String?> forRealtimeChatAddress({required String userId}) {
+    return db
+        .collection("realtimecash")
+        .doc(userId)
+        .snapshots()
+        .map((doc) => doc.exists ? doc.get("address") : null);
+  }
+
+  Future<void> setRealtimeCashAddress(
+      {required String userId, required String address}) {
+    return functions.httpsCallable("setRealtimeCashAddress")(address);
+  }
+
+  Stream<StreamlabsConfig?> forStreamlabsConfig({required String userId}) {
+    return db.collection("streamlabs").doc(userId).snapshots().map((doc) =>
+        doc.exists ? StreamlabsConfig(currency: doc.get("currency")) : null);
+  }
+}
