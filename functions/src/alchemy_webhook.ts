8d92d32f94246482da6696af09f0300b77ec66af
ChuangWei Ma <chungwwei223@gmail.com>
2022-08-30 14:07:49 -0400

    chore: error handling

--- functions/src/alchemy_webhook.ts
+++ functions/src/alchemy_webhook.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import fetch from "node-fetch";
 
 // TODO: webhook receiver from alchemy
 export const alchemyWebhook = functions.https.onRequest(async (req, res) => {
@@ -15,11 +16,22 @@ export const alchemyWebhook = functions.https.onRequest(async (req, res) => {
 });
 
 
+type webhookIdResponse = {
+	id: string;
+	appId: string;
+	network: string;
+	webhookType: string;
+	webHookUrl: string;
+	isActive: boolean;
+	timeCreated: string;
+	addresses: string[];
+}
+
 export const setRealTimeCashAddress = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError("permission-denied", "missing auth");
   }
-	// console.log("data", data);
+	
 	const userId = context.auth.uid;
 	const address = data?.address;
   if (!address) {
@@ -28,17 +40,39 @@ export const setRealTimeCashAddress = functions.https.onCall(async (data, contex
       "missing userId, address params"
     );
   }
-	// console.log('userId,', userId)
-	// console.log('address', address)
 
-	// hard-coded values for now
-	const webhookId = "wh_octjglnywaupz6th";
 
-	await admin.firestore().collection("realtimecash").doc(userId).set({
-		address,
-		webhookId,
-	}).catch(error => {
-		throw new functions.https.HttpsError(error, error.message);
-	});
+	// https://docs.alchemy.com/reference/create-webhook
+	// create webhook for user with userId
+	const options = {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'X-Alchemy-Token': 'this-is-a-token',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			network: 'ETH_MAINNET',
+			webhook_type: 'ADDRESS_ACTIVITY',
+			webhook_url: 'localhost/{userId}' // TODO: replace with real webhook url
+		})
+	};
+
+	fetch('https://dashboard.alchemyapi.io/api/create-webhook', options)
+		.then(response => response.json())
+		.then(async response => {
+			const res = response as webhookIdResponse;
+			const webhookId = res.id;
+			await admin.firestore().collection("realtimecash").doc(userId).set({
+				address,
+				webhookId,
+			}).catch(error => {
+				throw new functions.https.HttpsError(error, error.message);
+			});
+		})
+		.catch(err => {
+			throw new functions.https.HttpsError(err, err.message);
+		});
+
   return;
 });
