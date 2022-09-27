7b3173c95bd68fd567e631c2e0908c394ace0490
Kevin Wang <kevmo314@gmail.com>
2022-09-27 12:18:35 -0400

    chore: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
