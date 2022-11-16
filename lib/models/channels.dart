a02bd116e4018c19632ea25791613724134c44a2
Kevin Wang <kevmo314@gmail.com>
2022-11-16 17:55:28 -0500

    chore: read statistics from channels collection

--- lib/models/channels.dart
+++ lib/models/channels.dart
@@ -1,7 +1,5 @@
 import 'dart:core';
 
-import 'package:cloud_functions/cloud_functions.dart';
-
 class Channel {
   final String provider;
   final String channelId;
@@ -25,40 +23,4 @@ class Channel {
     return Uri.parse(
         "https://rtirl.com/pfp.png?provider=twitch&channelId=$channelId");
   }
-}
-
-class StreamMetadata {
-  final bool isOnline;
-  StreamMetadata({required this.isOnline});
-}
-
-class TwitchStreamMetadata extends StreamMetadata {
-  final int viewerCount;
-  final int followerCount;
-
-  TwitchStreamMetadata(
-      {required this.viewerCount,
-      required this.followerCount,
-      required bool isOnline})
-      : super(isOnline: isOnline);
-}
-
-final getStatistics = FirebaseFunctions.instance.httpsCallable("getStatistics");
-
-Future<StreamMetadata> getStreamMetadata(
-    {required String provider, required String channelId}) async {
-  final statistics = await getStatistics({
-    "provider": provider,
-    "channelId": channelId,
-  });
-
-  switch (provider) {
-    case "twitch":
-      return TwitchStreamMetadata(
-        isOnline: statistics.data['isOnline'] ?? false,
-        viewerCount: statistics.data['viewers'] ?? 0,
-        followerCount: statistics.data['followers'] ?? 0,
-      );
-  }
-  throw "invalid provider";
 }
