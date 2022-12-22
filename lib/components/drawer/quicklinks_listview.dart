757868e61e91248f81d2168ff39b800f5660581f
Kevin Wang <kevmo314@gmail.com>
2022-12-22 17:30:57 -0500

    chore: remove custom tabs and directly launch chrome

--- lib/components/drawer/quicklinks_listview.dart
+++ lib/components/drawer/quicklinks_listview.dart
@@ -4,22 +4,10 @@ import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:provider/provider.dart';
 import 'package:rtchat/models/quick_links.dart';
 import 'package:rtchat/screens/settings/quick_links.dart';
-import 'package:flutter_custom_tabs/flutter_custom_tabs.dart'
-    as flutter_custom_tabs;
-import 'package:url_launcher/url_launcher.dart' as url_launcher;
+import 'package:url_launcher/url_launcher.dart';
 
 class QuicklinksListView extends StatelessWidget {
   const QuicklinksListView({Key? key}) : super(key: key);
-
-  void launchLink(QuickLinkSource source) async {
-    final isWebUrl =
-        source.url.scheme == 'http' || source.url.scheme == 'https';
-    if (isWebUrl) {
-      await flutter_custom_tabs.launch(source.url.toString());
-    } else {
-      await url_launcher.launchUrl(source.url);
-    }
-  }
 
   Future<String> retrieveName(QuickLinkSource link) async {
     final metadata = await MetadataFetch.extract(link.url.toString());
@@ -41,7 +29,8 @@ class QuicklinksListView extends StatelessWidget {
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
             ),
-            onTap: () => launchLink(source),
+            onTap: () =>
+                launchUrl(source.url, mode: LaunchMode.externalApplication),
             onLongPress: () {
               Navigator.pop(context);
               Clipboard.setData(ClipboardData(text: url)).then((_) {
