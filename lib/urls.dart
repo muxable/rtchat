5566217b67d429b418d078b7e8c3c8194071c43b
Kevin Wang <kevmo314@gmail.com>
2023-01-11 01:04:52 -0500

    fix return type

--- lib/urls.dart
+++ lib/urls.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart' as customtabs;
 import 'package:url_launcher/url_launcher.dart';
 
-void openUrl(Uri url) async {
+Future<void> openUrl(Uri url) async {
   if (!url.hasScheme) {
     await customtabs.launch(url.replace(scheme: 'http').toString());
   } else if (url.isScheme("http") || url.isScheme("https")) {
