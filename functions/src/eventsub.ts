395dd4d4f2266c1e603543bff61339058872c5d1
ChuangWei Ma <39935368+chungwwei@users.noreply.github.com>
2023-01-20 16:40:03 -0500

    fix: shoutout request body incomplete (#889)
    * fix: shoutout request body incomplete
    
    * fix: stick to the condition needed for shoutout create and receive

--- functions/src/eventsub.ts
+++ functions/src/eventsub.ts
@@ -41,10 +41,18 @@ enum EventsubType {
 }
 
 function createEventsub(token: string, type: string, twitchUserId: string) {
-  const condition =
-    type == "channel.raid"
-      ? { to_broadcaster_user_id: twitchUserId }
-      : { broadcaster_user_id: twitchUserId };
+  var condition: any = { broadcaster_user_id: twitchUserId };
+  if (type === "channel.raid") {
+    condition = { to_broadcaster_user_id: twitchUserId };
+  } else if (
+    type === "channel.shoutout.create" ||
+    type === "channel.shoutout.receive"
+  ) {
+    condition = {
+      broadcaster_user_id: twitchUserId,
+      moderator_user_id: twitchUserId,
+    };
+  }
   const version =
     type === "channel.shoutout.create" || type === "channel.shoutout.receive"
       ? "beta"
