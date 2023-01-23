c118e83a18d022113c340f3540b90f7d2d70272e
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2023-01-23 06:28:02 -0800

    fix: spacing issues (#894)

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -185,15 +185,16 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           const Divider(),
           ListTile(
-                  title: const Text('RealtimeChat is open source!'),
-                  subtitle: const Text("Find us on GitHub!"),
-                  trailing: const Icon(Icons.terminal),
-            onTap: () => openUrl(Uri.parse("https://github.com/muxable/rtchat")),
+            title: const Text('RealtimeChat is open source!'),
+            subtitle: const Text("Find us on GitHub!"),
+            trailing: const Icon(Icons.terminal),
+            onTap: () =>
+                openUrl(Uri.parse("https://github.com/muxable/rtchat")),
           ),
           ListTile(
-                  title: const Text('Muxable Discord'),
-                  subtitle: const Text("Join the Muxable Discord!"),
-                  trailing: const Icon(Icons.launch),
+            title: const Text('Muxable Discord'),
+            subtitle: const Text("Join the Muxable Discord!"),
+            trailing: const Icon(Icons.launch),
             onTap: () => openUrl(Uri.parse("https://discord.gg/UKHJMQs74u")),
           ),
           Padding(
