29ba07373644fa1a343608e252348423131032e9
Kevin Wang <kevmo314@gmail.com>
2023-04-15 19:59:38 -0400

    feat: implement streamelements listener

--- auxiliary/src/streamelements.ts
+++ auxiliary/src/streamelements.ts
@@ -1,3 +1,4 @@
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 import fetch from "cross-fetch";
 import * as admin from "firebase-admin";
 import {
@@ -7,6 +8,7 @@ import {
   merge,
   mergeMap,
   Observable,
+  retry,
   switchMap,
   tap,
 } from "rxjs";
@@ -14,7 +16,7 @@ import {
 import io from "socket.io-client";
 import { log } from "./log";
 
-type Donation = {
+type Tip = {
   id: number;
   channelId: string;
   name: string;
@@ -24,6 +26,39 @@ type Donation = {
   currency: string;
   timestamp: Date;
 };
+
+async function refreshToken(refreshToken: string) {
+  const client = new SecretManagerServiceClient();
+  const [clientIdVersion] = await client.accessSecretVersion({
+    name: "projects/rtchat-47692/secrets/streamelements-client-id/versions/latest",
+  });
+  const clientIdSecret = clientIdVersion.payload?.data?.toString();
+  if (!clientIdSecret) {
+    throw new Error("unable to retrieve credentials from secret manager");
+  }
+  const [clientSecretVersion] = await client.accessSecretVersion({
+    name: "projects/rtchat-47692/secrets/streamelements-client-secret/versions/latest",
+  });
+  const clientSecretSecret = clientSecretVersion.payload?.data?.toString();
+  if (!clientSecretSecret) {
+    throw new Error("unable to retrieve credentials from secret manager");
+  }
+  const response = await fetch(`https://api.streamelements.com/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+      client_id: clientIdSecret,
+      client_secret: clientSecretSecret,
+    }),
+  });
+  const json = await response.json();
+  const expiresAt = new Date(+new Date() + json["expires_in"] * 1000);
+  return { ...json, expires_at: expiresAt.toISOString() };
+}
 
 function listenForSingleUserId(userId: string) {
   log.info({ userId }, "checking if streamelements linked");
@@ -45,88 +80,128 @@ function listenForSingleUserId(userId: string) {
       if (!token || !channelId) {
         return new Observable<never>((subscriber) => subscriber.complete());
       }
-      const accessToken = JSON.parse(token)?.["access_token"];
+      const parsed = JSON.parse(token);
+      if (!parsed) {
+        return new Observable<never>((subscriber) => subscriber.complete());
+      }
+      const accessToken = parsed["access_token"];
       if (!accessToken) {
         return new Observable<never>((subscriber) => subscriber.complete());
       }
+      // check the expiration date.
+      const expiresAt = Date.parse(parsed["expires_at"]);
+      if (expiresAt < +new Date() + 1000 * 60 * 60 * 24) {
+        // refresh the token.
+        log.info({ userId }, "refreshing streamelements token");
+        refreshToken(parsed["refresh_token"]).then((newToken) => {
+          return doc.ref.update({ token: JSON.stringify(newToken) });
+        });
+        return new Observable<never>((subscriber) => subscriber.complete());
+      }
+
       // log a message to indicate that we're listening for donations.
       log.info({ userId, channelId }, "listening for donations");
       const after = doc.get("after");
-      const currency = doc.get("currency");
       async function* poll() {
-        let url = `https://api.streamelements.com/kappa/v2/tips/`;
+        console.log(accessToken);
+        const channel = await fetch(
+          `https://api.streamelements.com/kappa/v2/channels/me`,
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `OAuth ${accessToken}`,
+            },
+          }
+        )
+          .then((response) => response.json())
+          .then((json) => json["_id"]);
+        let url = `https://api.streamelements.com/kappa/v2/tips/${channel}`;
         if (after) {
           url += `&after=${after}`;
         }
-        if (currency) {
-          url += `&currency=${currency}`;
-        }
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `OAuth ${accessToken}`,
+          },
+        });
         const json = await response.json();
-        if (!json["data"]) {
+        if (!json["docs"]) {
           return;
         }
-        for (const donation of json["data"]) {
+        for (const doc of json["docs"]) {
           const formatter = new Intl.NumberFormat("en-US", {
             style: "currency",
-            currency: donation["currency"],
+            currency: doc["donation"]["currency"],
           });
-          yield <Donation>{
-            id: donation["donation_id"],
+          yield <Tip>{
+            id: doc["_id"],
             channelId,
-            name: donation["name"],
-            amount: donation["amount"],
-            formattedAmount: formatter.format(donation["amount"]),
-            message: donation["message"],
-            currency: donation["currency"],
-            timestamp: new Date(donation["created_at"] * 1000),
+            name: doc["donation"]["user"]["username"],
+            amount: doc["donation"]["amount"],
+            formattedAmount: formatter.format(doc["donation"]["amount"]),
+            message: doc["donation"]["message"],
+            currency: doc["donation"]["currency"],
+            timestamp: new Date(doc["createdAt"]),
           };
         }
       }
-      async function socketToken() {
-        const response = await fetch(
-          `https://streamelements.com/api/v1.0/socket/token?access_token=${accessToken}`
-        );
-        const json = await response.json();
-        return json["socket_token"] as string;
+      function push() {
+        // if there's no token, return an empty observable.
+        return new Observable<Tip>((subscriber) => {
+          const streamelements = io(`https://realtime.streamelements.com/`, {
+            transports: ["websocket"],
+          });
+          streamelements.on("connect", () => {
+            log.info({ userId }, "connected to streamelements");
+            streamelements.emit("authenticate", {
+              method: "oauth2",
+              token: accessToken,
+            });
+          });
+          streamelements.on("event", (eventData: any) => {
+            if (eventData["type"] === "tip") {
+              const tip = eventData["data"];
+              const formatter = new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: tip["currency"],
+              });
+              subscriber.next(<Tip>{
+                id: eventData["_id"],
+                channelId,
+                name: tip["username"],
+                amount: tip["amount"],
+                formattedAmount: formatter.format(tip["amount"]),
+                message: tip["message"],
+                currency: tip["currency"],
+                timestamp: new Date(Date.parse(eventData["createdAt"])),
+              });
+            }
+          });
+          streamelements.on("authenticated", () => {
+            log.info({ userId }, "authenticated with streamelements");
+          });
+          streamelements.on("disconnect", () => {
+            log.error({ userId }, "unexpected disconnect from streamelements");
+            subscriber.error(
+              new Error("unexpected disconnect from streamelements")
+            );
+          });
+          streamelements.on("unauthorized", (reason: any) => {
+            log.error({ userId, reason }, "unauthorized from streamelements");
+            subscriber.error(
+              new Error("unauthorized from streamelements: " + reason)
+            );
+          });
+          return () => streamelements.disconnect();
+        });
       }
       return merge(
         // return an observable that starts by polling for any missed donations.
         from(poll()),
         // and merge with a socket that listens for new donations.
-        from(socketToken()).pipe(
-          mergeMap((token) => {
-            return new Observable<Donation>((subscriber) => {
-              if (!token) {
-                subscriber.error("no token");
-                return;
-              }
-              const streamelements = io(
-                `https://sockets.streamelements.com/?token=${token}`,
-                {
-                  transports: ["websocket"],
-                }
-              );
-              streamelements.on("event", (eventData: any) => {
-                if (!eventData.for && eventData.type === "donation") {
-                  for (const donation of eventData.message) {
-                    subscriber.next({
-                      id: donation.id,
-                      channelId,
-                      name: donation.name,
-                      amount: donation.amount,
-                      formattedAmount: donation.formatted_amount,
-                      message: donation.message,
-                      currency: donation.currency,
-                      timestamp: new Date(),
-                    });
-                  }
-                }
-              });
-              return () => streamelements.disconnect();
-            });
-          })
-        )
+        from(push().pipe(retry(5)))
       ).pipe(
         tap((donation) => {
           // write the after value to the database.
@@ -134,7 +209,7 @@ function listenForSingleUserId(userId: string) {
             const streamelementsDoc = await tx.get(doc.ref);
             const after = streamelementsDoc.get("after");
             if (!after || after < donation.id) {
-              tx.update(doc.ref, { after: donation.id });
+              tx.update(doc.ref, { after: donation.timestamp.getTime() });
             }
           });
         })
@@ -154,7 +229,7 @@ function listenForSingleUserId(userId: string) {
         .doc(`streamelements-${donation.channelId}-${donation.id}`)
         .set({
           ...donation,
-          type: "streamelements.donation",
+          type: "streamelements.tip",
           timestamp: admin.firestore.Timestamp.fromDate(donation.timestamp),
           expiresAt: admin.firestore.Timestamp.fromMillis(
             Date.now() + 1000 * 86400 * 7 * 2
