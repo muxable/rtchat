70d6346def20b3cd02c344457ad91772e55af83f
Kevin Wang <kevmo314@gmail.com>
2023-09-16 08:00:00 -0400

    Revert "feat: use twitch irc for sending messages (#1067)" (#1072)
    This reverts commit aec791b907a4ba97888882c662be6bdab2e348ef.
    
    Co-authored-by: SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>

--- lib/models/adapters/actions.dart
+++ lib/models/adapters/actions.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:rtchat/models/channels.dart';
 
 class ActionsAdapter {
@@ -13,13 +14,23 @@ class ActionsAdapter {
       functions: FirebaseFunctions.instance);
   static ActionsAdapter? _instance;
 
-  Future<void> send(Channel channel, String message) async {
+  Future<String?> send(Channel channel, String message) async {
     final call = functions.httpsCallable('send');
-    await call({
-      "provider": channel.provider,
-      "channelId": channel.channelId,
-      "message": message,
-    });
+    final key = firestore.collection('actions').doc().id;
+    for (var i = 0; i < 3; i++) {
+      try {
+        final result = await call({
+          "id": key,
+          "provider": channel.provider,
+          "channelId": channel.channelId,
+          "message": message,
+        });
+        return result.data;
+      } catch (e) {
+        FirebaseCrashlytics.instance.recordError(e, StackTrace.current);
+      }
+    }
+    throw Exception("Failed to send message");
   }
 
   Future<void> ban(Channel channel, String username) async {
