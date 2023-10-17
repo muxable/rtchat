8fffd93eaaf0b2fd4ee97329e7bf473c800ab083
jlw4049 <jlw_4049@hotmail.com>
2023-10-17 18:48:36 -0400

    feat: create the groundwork for a native android tts channel

--- android/app/src/main/kotlin/com/rtirl/chat/MainActivity.kt
+++ android/app/src/main/kotlin/com/rtirl/chat/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.rtirl.chat
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.speech.tts.TextToSpeech
 import android.os.Build
 import android.provider.Settings
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 
 
 class MainActivity : FlutterActivity() {
@@ -62,5 +67,25 @@ class MainActivity : FlutterActivity() {
         }
 
         super.configureFlutterEngine(flutterEngine)
+    }
+}
+
+
+class TTSPlugin(context: Context) : MethodCallHandler {
+    private val context: Context = context
+    private val tts: TextToSpeech = TextToSpeech(context) {}
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "speak") {
+            val text = call.argument<String>("text")
+            if (!text.isNullOrBlank()) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+                result.success(true)
+            } else {
+                result.error("INVALID_ARGUMENT", "Text is empty or null", null)
+            }
+        } else {
+            result.notImplemented()
+        }
     }
 }
---
+++ lib/tts_plugin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/services.dart';
+
+class TTSPlugin {
+  static const MethodChannel _channel = MethodChannel('tts_plugin');
+
+  static Future<void> speak(String text) async {
+    try {
+      await _channel.invokeMethod('speak', {'text': text});
+    } catch (e) {
+      // TODO Handle the error?
+      // print('Error in TTSPlugin: $e');
+    }
+  }
+}
