d8281c831f44e3040ae8ac08d3300591005e015e
Jessie Wilson <48299282+jlw4049@users.noreply.github.com>
2023-10-18 14:08:10 -0400

    Update lib/tts_plugin.dart
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>

--- lib/tts_plugin.dart
+++ lib/tts_plugin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 
-class TTSPlugin {
+class TextToSpeechPlugin {
   static const MethodChannel _channel = MethodChannel('tts_plugin');
 
   static Future<void> speak(String text) async {
