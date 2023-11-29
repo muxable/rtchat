ba3575ae7668f8b4593bc5e66021f0c7720b30d0
jlw4049 <jlw_4049@hotmail.com>
2023-11-29 14:43:51 -0500

    test: add a test that resolving speak from the mocked channel lines up the next message

--- test/models/tts_test.dart
+++ test/models/tts_test.dart
@@ -3,7 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rtchat/tts_plugin.dart';
 
+class MockMethodChannel extends Mock implements MethodChannel {}
+
+class MockTTSQueue extends Mock implements TTSQueue {
+  @override
+  Future<void> speak(String? id, String? text) =>
+      super.noSuchMethod(Invocation.method(#speak, [id, text]),
+          returnValue: Future.value());
+}
+
 void main() {
+  final mockedTTSQueue = MockTTSQueue();
+
   group('TTSQueue', () {
     late TTSQueue ttsQueue;
     late MockMethodChannel mockChannel;
@@ -43,13 +54,24 @@ void main() {
     test('Delete removes the element by id', () async {
       ttsQueue.speak('1', 'First message');
       ttsQueue.speak('2', 'Second message');
-
       ttsQueue.delete('1');
-
       expect(ttsQueue.length, equals(1));
       expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
     });
+
+    test('Resolving speak from the mocked channel lines up the next message',
+        () async {
+      ttsQueue.speak('1', 'First message');
+      ttsQueue.speak('2', 'Second message');
+
+      when(mockedTTSQueue.speak(any, any)).thenAnswer((_) async {});
+
+      await ttsQueue.speakNext();
+      expect(ttsQueue.length, equals(1));
+      expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
+
+      await ttsQueue.speakNext();
+      expect(ttsQueue.isEmpty, isTrue);
+    });
   });
 }
-
-class MockMethodChannel extends Mock implements MethodChannel {}
