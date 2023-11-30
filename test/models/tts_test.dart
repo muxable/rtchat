ce3902b261a29bdcff3141b1ea4ed44ffb2048de
jlw4049 <jlw_4049@hotmail.com>
2023-11-30 15:29:04 -0500

    refactor: removed mockito refactor: still a WIP (final test is still broken), cannot get last test to work properly or for the flutter test to interact with the method channel, it will never go into the handler.

--- test/models/tts_test.dart
+++ test/models/tts_test.dart
@@ -1,83 +1,80 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:rtchat/tts_plugin.dart';
 
-class MockMethodChannel extends Mock implements MethodChannel {}
-
-class MockTTSQueue extends Mock implements TTSQueue {
-  @override
-  Future<void> speak(String? id, String? text) =>
-      super.noSuchMethod(Invocation.method(#speak, [id, text]),
-          returnValue: Future.value());
-}
-
 void main() {
-  final mockedTTSQueue = MockTTSQueue();
+  const channel = MethodChannel('tts_plugin');
+  final ttsQueue = TTSQueue(channel);
 
-  group('TTSQueue', () {
-    late TTSQueue ttsQueue;
-    late MockMethodChannel mockChannel;
+  test('Queue starts empty', () {
+    expect(ttsQueue.isEmpty, isTrue);
+  });
 
-    setUp(() {
-      mockChannel = MockMethodChannel();
-      ttsQueue = TTSQueue(mockChannel);
-    });
+  test('Speak adds elements to the queue', () async {
+    ttsQueue.speak('1', 'First message');
+    ttsQueue.speak('2', 'Second message');
+    await ttsQueue.speakNext();
+    expect(ttsQueue.isEmpty, isFalse);
+    expect(ttsQueue.length, equals(1));
+  });
 
-    test('Queue starts empty', () {
-      expect(ttsQueue.isEmpty, isTrue);
-    });
+  test('Speak and clear empties the queue', () async {
+    ttsQueue.speak('1', 'First message');
+    ttsQueue.speak('2', 'Second message');
+    await ttsQueue.clear();
+    expect(ttsQueue.isEmpty, isTrue);
+  });
 
-    test('Speak adds elements to the queue', () async {
-      ttsQueue.speak('1', 'First message');
-      ttsQueue.speak('2', 'Second message');
-      await ttsQueue.speakNext();
-      expect(ttsQueue.isEmpty, isFalse);
-      expect(ttsQueue.length, equals(1));
-    });
+  test('SpeakNext removes the first element from the queue', () async {
+    ttsQueue.speak('1', 'First message');
+    ttsQueue.speak('2', 'Second message');
+    await ttsQueue.speakNext();
+    expect(ttsQueue.length, equals(1));
+    expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
+  });
 
-    test('Speak and clear empties the queue', () async {
-      ttsQueue.speak('1', 'First message');
-      ttsQueue.speak('2', 'Second message');
-      await ttsQueue.clear();
-      expect(ttsQueue.isEmpty, isTrue);
-    });
+  test('Delete removes the element by id', () async {
+    ttsQueue.speak('1', 'First message');
+    ttsQueue.speak('2', 'Second message');
+    await ttsQueue.delete('1');
+    expect(ttsQueue.length, equals(1));
+    expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
+  });
 
-    test('SpeakNext removes the first element from the queue', () async {
-      ttsQueue.speak('1', 'First message');
-      ttsQueue.speak('2', 'Second message');
-      await ttsQueue.speakNext();
-      expect(ttsQueue.length, equals(1));
-      expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
-    });
+  test('Resolving speak from the mocked channel lines up the next message',
+      () async {
+    int callCount = 0;
 
-    test('Delete removes the element by id', () async {
-      ttsQueue.speak('1', 'First message');
-      ttsQueue.speak('2', 'Second message');
-      ttsQueue.delete('1');
-      expect(ttsQueue.length, equals(1));
-      expect(ttsQueue.peek(), equals({'id': '2', 'text': 'Second message'}));
-    });
-
-    test('Resolving speak from the mocked channel lines up the next message',
-        () async {
-      var callCount = 0;
-      when(mockedTTSQueue.speak(any, any)).thenAnswer((_) async {
+    handler(MethodCall methodCall) async {
+      print('were in the test2');
+      if (methodCall.method == 'speak') {
         callCount++;
-        await Future.delayed(seconds: 1);  // prevent race conditions
-      });
+        await Future.delayed(Duration(seconds: 1));
+        return null;
+      }
+      return null;
+    }
 
-      expect(callCount, 0);
-      const promise1 = ttsQueue.speak('1', 'First message');
-      const promise2 = ttsQueue.speak('2', 'Second message');
-      
-      expect(ttsQueue.length, equals(1));  // pop off immediately
-      await promise1;
-      expect(callCount, 1);
-      expect(ttsQueue.length, equals(1));  // resolve before the next tts speak command has finished
-      await promise2;
-      expect(callCount, 2);
-      expect(ttsQueue.length, equals(0)); 
-    });
+    TestWidgetsFlutterBinding.ensureInitialized();
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, handler);
+
+    expect(callCount, 0);
+
+    final promise1 = ttsQueue.speak('1', 'First message');
+    final promise2 = ttsQueue.speak('2', 'Second message');
+
+    expect(ttsQueue.length, equals(1));
+
+    await promise1;
+
+    expect(callCount, 1);
+    expect(ttsQueue.length, equals(1));
+
+    await promise2;
+
+    expect(callCount, 2);
+
+    expect(ttsQueue.length, equals(0));
   });
 }
