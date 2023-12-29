26fe5fc075b27c17c5755dcea5b93f2b4d1ffbe4
Jessie Wilson <48299282+jlw4049@users.noreply.github.com>
2023-12-29 11:44:00 -0500

    Isolate background service (#1147)
    * refactor: added flutter_background_service
    
    * refactor: upgrade compiledsdk to version 34
    
    * feat: setup isolate to be used as a background service
    
    ---------
    
    Co-authored-by: SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>

--- lib/tts_isolate.dart
+++ lib/tts_isolate.dart
@@ -1,7 +1,56 @@
 import 'dart:isolate';
+import 'dart:ui';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 
 @pragma("vm:entry-point")
 void isolateMain(SendPort sendPort) {
-  print("Hello from the isolate");
-  sendPort.send("Isolate message");
+  initializeService(sendPort);
+}
+
+void initializeService(SendPort sendPort) async {
+  final service = FlutterBackgroundService();
+
+  void onStart(ServiceInstance service) async {
+    DartPluginRegistrant.ensureInitialized();
+    if (service is AndroidServiceInstance) {
+      service.on('setAsForeground').listen((event) {
+        service.setAsForegroundService();
+      });
+      service.on('setAsBackground').listen((event) {
+        service.setAsBackgroundService();
+      });
+    }
+    service.on('stopService').listen((event) {
+      service.stopSelf();
+    });
+    // Some code for background task
+  }
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: (ServiceInstance service) {
+        // Send a message to the main isolate
+        sendPort.send("Background service started");
+
+        // Perform background tasks here...
+        onStart(service);
+      },
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      autoStart: true,
+      onForeground: (ServiceInstance service) {
+        // Send a message to the main isolate
+        sendPort.send("Background service started");
+
+        // Perform background tasks here...
+        onStart(service);
+      },
+    ),
+  );
+
+  // Start the background service
+  service.startService();
 }
