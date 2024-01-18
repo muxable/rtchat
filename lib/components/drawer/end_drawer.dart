a854e2f0144368f59fb055d90ebe92be453be8c1
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-01-18 00:39:54 -0800

    fix: feedback

--- lib/components/drawer/end_drawer.dart
+++ lib/components/drawer/end_drawer.dart
@@ -1,141 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-// import 'package:rtchat/components/drawer/sliver_search_bar.dart';
-// import 'package:rtchat/components/drawer/sliver_title.dart';
-// import 'package:rtchat/models/adapters/chat_state.dart';
-// import 'package:rtchat/models/channels.dart';
-//
-// class EndDrawerWidget extends StatefulWidget {
-//   final Channel channel;
-//
-//   const EndDrawerWidget({required this.channel, Key? key}) : super(key: key);
-//
-//   @override
-//   State<EndDrawerWidget> createState() => EndDrawerWidgetState();
-// }
-//
-// class EndDrawerWidgetState extends State<EndDrawerWidget> {
-//   String _search = "";
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Theme.of(context).canvasColor,
-//       child: FutureBuilder<Viewers>(
-//         future: ChatStateAdapter.instance
-//             .getViewers(channelId: widget.channel.toString()),
-//         builder: (context, snapshot) {
-//           final viewers = snapshot.data;
-//           if (viewers == null) {
-//             return const Center(child: CircularProgressIndicator());
-//           }
-//           final filtered = viewers.query(_search);
-//           return SafeArea(
-//             top: false,
-//             child: CustomScrollView(
-//               slivers: [
-//                 SliverPadding(
-//                   padding: const EdgeInsets.only(top: 24),
-//                   sliver: SliverAppBar(
-//                       actions: const [SizedBox()],
-//                       //disable the drawer icon that appears on the right of the app bar
-//                       centerTitle: false,
-//                       title: Padding(
-//                         padding: const EdgeInsets.only(left: 16),
-//                         child: Text(
-//                           AppLocalizations.of(context)!.searchViewers,
-//                           style: Theme.of(context)
-//                               .textTheme
-//                               .headlineSmall
-//                               ?.copyWith(fontWeight: FontWeight.bold),
-//                         ),
-//                       ),
-//                       backgroundColor: Colors.transparent,
-//                       automaticallyImplyLeading: false),
-//                 ),
-//                 SliverSearchBarWidget(
-//                   onFilterBySearchBarText: (value) =>
-//                       setState(() => _search = value),
-//                 ),
-//                 if (filtered.broadcaster.isNotEmpty) ...[
-//                   SliverPadding(
-//                     padding: const EdgeInsets.only(top: 8, left: 8),
-//                     sliver: SliverTitleWidget(
-//                         title: AppLocalizations.of(context)!.broadcaster),
-//                   ),
-//                   SliverList(
-//                     delegate: SliverChildListDelegate(
-//                       filtered.broadcaster.map((name) {
-//                         return Padding(
-//                           padding: const EdgeInsets.only(left: 24),
-//                           child: Text(name),
-//                         );
-//                       }).toList(),
-//                     ),
-//                   ),
-//                 ],
-//                 if (filtered.moderators.isNotEmpty) ...[
-//                   SliverPadding(
-//                     padding: const EdgeInsets.only(top: 8, left: 8),
-//                     sliver: SliverTitleWidget(
-//                         title: AppLocalizations.of(context)!.moderators),
-//                   ),
-//                   SliverList(
-//                     delegate: SliverChildListDelegate(
-//                       filtered.moderators.map((name) {
-//                         return Padding(
-//                           padding: const EdgeInsets.only(left: 24),
-//                           child: Text(name),
-//                         );
-//                       }).toList(),
-//                     ),
-//                   ),
-//                 ],
-//                 if (filtered.vips.isNotEmpty) ...[
-//                   SliverPadding(
-//                     padding: const EdgeInsets.only(top: 8, left: 8),
-//                     sliver: SliverTitleWidget(
-//                         title: AppLocalizations.of(context)!.communityVips),
-//                   ),
-//                   SliverList(
-//                     delegate: SliverChildListDelegate(
-//                       filtered.vips.map((name) {
-//                         return Padding(
-//                           padding: const EdgeInsets.only(left: 24),
-//                           child: Text(name),
-//                         );
-//                       }).toList(),
-//                     ),
-//                   ),
-//                 ],
-//                 if (filtered.viewers.isNotEmpty) ...[
-//                   SliverPadding(
-//                     padding: const EdgeInsets.only(top: 8, left: 8),
-//                     sliver: SliverTitleWidget(
-//                         title: AppLocalizations.of(context)!.viewers),
-//                   ),
-//                   SliverList(
-//                     delegate: SliverChildListDelegate(
-//                       filtered.viewers.map((name) {
-//                         return Padding(
-//                           padding: const EdgeInsets.only(left: 24),
-//                           child: Text(name),
-//                         );
-//                       }).toList(),
-//                     ),
-//                   ),
-//                 ],
-//               ],
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:rtchat/components/drawer/sliver_search_bar.dart';
 import 'package:rtchat/components/drawer/sliver_title.dart';
 import 'package:rtchat/models/adapters/chat_state.dart';
@@ -144,7 +8,10 @@ import 'package:rtchat/models/channels.dart';
 class EndDrawerWidget extends StatefulWidget {
   final Channel channel;
 
-  const EndDrawerWidget({required this.channel, super.key});
+  final Function(String) onError;
+
+  const EndDrawerWidget(
+      {required this.channel, super.key, required this.onError});
 
   @override
   State<EndDrawerWidget> createState() => EndDrawerWidgetState();
@@ -152,56 +19,37 @@ class EndDrawerWidget extends StatefulWidget {
 
 class EndDrawerWidgetState extends State<EndDrawerWidget> {
   String _search = "";
+  Viewers? _viewers;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadViewers();
+  }
+
+  void _loadViewers() async {
+    try {
+      final viewers = await ChatStateAdapter.instance
+          .getViewers(channelId: widget.channel.toString());
+      setState(() => _viewers = viewers);
+    } catch (e) {
+      widget.onError(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Check if viewers data is available and build UI accordingly
+    if (_viewers == null) {
+      // Show loading or empty state
+      return const CircularProgressIndicator();
+    }
+
+    final filtered = _viewers!.query(_search);
+
     return Container(
-      color: Theme.of(context).canvasColor,
-      child: FutureBuilder<Viewers>(
-        future: ChatStateAdapter.instance.getViewers(channelId: widget.channel.toString()),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-
-            Fluttertoast.showToast(
-              msg: "Loading data, please wait...",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.blue,
-              textColor: Colors.white,
-              fontSize: 10.0,
-            );
-            return Container();
-          } else if (snapshot.hasError) {
-
-            Fluttertoast.showToast(
-              msg: "An error occurred. Please try again later.",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.blue,
-              textColor: Colors.white,
-              fontSize: 10.0,
-            );
-            return Container();
-          }
-
-          final viewers = snapshot.data;
-          if (viewers == null) {
-            Fluttertoast.showToast(
-              msg: "An error occurred. Please try again later.",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.blue,
-              textColor: Colors.white,
-              fontSize: 10.0,
-            );
-            return Container();
-          }
-
-          final filtered = viewers.query(_search);
-          return SafeArea(
+        color: Theme.of(context).canvasColor,
+        child: SafeArea(
             top: false,
             child: CustomScrollView(
               slivers: [
@@ -225,7 +73,8 @@ class EndDrawerWidgetState extends State<EndDrawerWidget> {
                   ),
                 ),
                 SliverSearchBarWidget(
-                  onFilterBySearchBarText: (value) => setState(() => _search = value),
+                  onFilterBySearchBarText: (value) =>
+                      setState(() => _search = value),
                 ),
                 if (filtered.broadcaster.isNotEmpty) ...[
                   SliverPadding(
@@ -296,10 +145,6 @@ class EndDrawerWidgetState extends State<EndDrawerWidget> {
                   ),
                 ],
               ],
-            ),
-          );
-        },
-      ),
-    );
+            )));
   }
 }
