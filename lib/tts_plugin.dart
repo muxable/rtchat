9c28b2f95f2bb83f658108c587c1c8764a076000
jlw4049 <jlw_4049@hotmail.com>
2024-02-09 13:38:24 -0500

    refactor: this should call clear after stop speaking to ensure the queue is cleared, let this handle it in the plugin vs calling it explicitly in other parts of the app

--- lib/tts_plugin.dart
+++ lib/tts_plugin.dart
@@ -77,6 +77,7 @@ class TTSQueue {
     queue.clear();
     try {
       await TextToSpeechPlugin.stopSpeaking();
+      await TextToSpeechPlugin.clear();
     } catch (e) {
       // handle the error;
     }
