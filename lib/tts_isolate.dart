6b5ade522f7ecccb68def6f577cf8dc138afbf35
jlw4049 <jlw_4049@hotmail.com>
2024-02-09 13:37:49 -0500

    refactor: remove the reliance on shared_preferences since we can directly send messages through the isolate refactor: commented out code that i believe shouldn't be in here/used, wait for something to confirm, but form reading you shouldn't call flutter_background_service_android from an isolate (this is all done for you with the default flutter_background_service) feat: hook up tts queue system fix: queue messages being processed in the back ground even if text to speech was disabled/first launch of app feat: added a listener function that handles messages from the firebase correctly, but also will kill the listener once TTS is disabled feat: created a shared class that toggles the TTS for the isolate that can be sent through the background service via the invoke method

--- lib/tts_isolate.dart
+++ lib/tts_isolate.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:isolate';
 import 'dart:ui';
 
@@ -5,14 +6,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+// import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
-import 'package:stream_transform/stream_transform.dart';
 import 'package:rtchat/tts_plugin.dart';
+
+final ttsQueue = TTSQueue();
+bool ttsEnabled = false;
+StreamSubscription? ttsListenerSubscription;
+String ttsCurrentChannel = '';
 
 @pragma("vm:entry-point")
 void isolateMain(SendPort sendPort) {
-  // print("Hello from the isolate");
   sendPort.send("Isolate message");
 }
 
@@ -52,44 +56,29 @@ void onStart(ServiceInstance service) async {
   DartPluginRegistrant.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // final prefs = await StreamingSharedPreferences.instance;
 
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) async {
-      service.setAsForegroundService();
-      // Setting up the notification at the very beginning
-    });
+  // if (service is AndroidServiceInstance) {
+  //   service.on('setAsForeground').listen((event) async {
+  //     service.setAsForegroundService();
+  //     // Setting up the notification at the very beginning
+  //   });
 
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
-    });
-  }
+  //   service.on('setAsBackground').listen((event) {
+  //     service.setAsBackgroundService();
+  //   });
+  // }
 
   // Handle other service events
-  service.on('stopService').listen((event) {
-    // print("Stopping this service right now");
-    TextToSpeechPlugin.stopSpeaking();
+  service.on('stopTts').listen((event) async {
+    await toggleTts(event);
+    await ttsQueue.clear();
   });
 
   service.on('startTts').listen((event) async {
     await Isolate.spawn(isolateMain, ReceivePort().sendPort);
-  });
-
-  final prefs = await StreamingSharedPreferences.instance;
-  prefs.getString('tts_channel', defaultValue: '{}').switchMap((channel) {
-    if (channel.isNotEmpty && channel != "{}") {
-      return FirebaseFirestore.instance
-          .collection('channels')
-          .where('channelId', isEqualTo: channel)
-          .snapshots();
-    } else {
-      return const Stream.empty();
-    }
-  }).listen((snapshot) {
-    for (final change in snapshot.docChanges) {
-      if (change.type == DocumentChangeType.added) {
-        vocalizeMessage(change.doc.data());
-      }
-    }
+    await toggleTts(event);
+    await setupTtsListener();
   });
 
   /// you can see this log in logcat
@@ -104,35 +93,74 @@ void onStart(ServiceInstance service) async {
   );
 }
 
+Future<void> toggleTts(event) async {
+  if (event != null) {
+    Map<String, dynamic>? args = event as Map<String, dynamic>?;
+    ttsEnabled = args?['toggle'] ?? false;
+    if (args?['channel'] != null) {
+      var channelData = args!["channel"];
+      ttsCurrentChannel =
+          "${channelData['provider']}:${channelData['channelId']}";
+    } else {
+      ttsCurrentChannel = '';
+    }
+  }
+}
+
+Future<void> setupTtsListener() async {
+  ttsListenerSubscription = FirebaseFirestore.instance
+      .collection('channels')
+      .doc(ttsCurrentChannel)
+      .collection('messages')
+      .orderBy('timestamp', descending: true)
+      .limit(1)
+      .snapshots()
+      .listen((latestMessage) async {
+    if (!ttsEnabled) {
+      ttsListenerSubscription?.cancel();
+      return;
+    }
+    if (latestMessage.docs.isNotEmpty) {
+      final timestamp = latestMessage.docs[0]['timestamp'];
+      final userId = latestMessage.docs[0]['author']["userId"];
+      final uniqueId = '$timestamp-$userId';
+      final textToSpeak = latestMessage.docs[0]['message'] as String?;
+      if (textToSpeak != null) {
+        await ttsQueue.speak(uniqueId, textToSpeak);
+      }
+    }
+  });
+}
+
 Future<void> resetTTS() async {
   TextToSpeechPlugin.stopSpeaking();
   final prefs = await StreamingSharedPreferences.instance;
   prefs.setString('tts_channel', '{}');
 }
 
-void vocalizeMessage(Map<String, dynamic>? message) async {
-  StreamingSharedPreferences prefs = await StreamingSharedPreferences.instance;
-  if (message == null) {
-    // print("Received a null message");
-    return;
-  }
+// void vocalizeMessage(Map<String, dynamic>? message) async {
+//   StreamingSharedPreferences prefs = await StreamingSharedPreferences.instance;
+//   if (message == null) {
+//     // print("Received a null message");
+//     return;
+//   }
 
-  var textToSpeak = message['text'] as String?;
+//   var textToSpeak = message['text'] as String?;
 
-  var isOnline = message['isOnline'] as bool? ?? false;
+//   var isOnline = message['isOnline'] as bool? ?? false;
 
-  if (!isOnline) {
-    debugPrint("Received a message from an offline user");
-    TextToSpeechPlugin.stopSpeaking();
-    prefs.remove('tts_channel');
-  }
-  if (textToSpeak != null) {
-    try {
-      TextToSpeechPlugin.speak(textToSpeak);
-    } catch (e) {
-      debugPrint("Error during TTS processing: $e");
-    }
-  } else {
-    debugPrint("Received a message with null or missing 'text' field");
-  }
-}
+//   if (!isOnline) {
+//     debugPrint("Received a message from an offline user");
+//     TextToSpeechPlugin.stopSpeaking();
+//     prefs.remove('tts_channel');
+//   }
+//   if (textToSpeak != null) {
+//     try {
+//       TextToSpeechPlugin.speak(textToSpeak);
+//     } catch (e) {
+//       debugPrint("Error during TTS processing: $e");
+//     }
+//   } else {
+//     debugPrint("Received a message with null or missing 'text' field");
+//   }
+// }
