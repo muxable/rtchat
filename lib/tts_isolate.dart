db2dfe9130a41b420037db4abd732141b2bd04ec
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-02-26 13:33:17 -0800

    fix: disable if tts gets too long (#1195)
    * fix: disable if tts gets too long
    
    * fix: move to other thread
    
    * fix: disable queue if too long with test
    
    * fix: test
    
    * fix: unused import
    
    * fix: feedback
    
    * refactor: make ttsChannel nullable and typed instead of a string
    refactor: setup ttsChannel to be a getter, defaulting to true
    
    * refactor: stripped out shared preference code, this is equal to main branch now and allows the build to compile
    
    * refactor: bring up to speed with main branch
    
    * fix: warnings
    
    * fix: sidebar.dart
    
    * fix: sidebar.dart
    
    * fix: home.dart
    
    * fix: audio_sources.dart
    
    * fix: home.dart
    
    * fix: format
    
    * fix: comment
    
    * fix: analyzer
    
    * refactor: remove flutter_background_service_android
    
    * refactor: remove unused code
    
    * fix: completer not removing items from queue properly
    
    * chore: remove part of the tts engine code (sure this needs worked on more)
    
    * refactor: remove un-needed code
    
    * chore: remove flutter_background_service
    
    * refactor: renamed the plugin channel
    
    * feat: add a function to update the channel stream, the background isolate listens to changes in this stream
    
    * chore: removed flutter_background_services
    refactor: import main.dart to enable channel swapping via it's function updateChannelSubscription
    fix: queued text to speech not working correctly
    
    * refactor: re write to the queue class to ensure that speech is completed before going to the next item in the queue
    fix: fixed an error where it would sometimes delete the item that was next in queue
    feat: create a ttsqueueelement class to be used throughout our main queue class as needed
    fix: disabling of text to speech when messages are greater than 20
    
    * feat: re wrote hte isolate to use flutters built in isolate
    
    * Update lib/models/tts.dart
    
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>
    
    * Update lib/models/tts.dart
    
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>
    
    * Update lib/tts_isolate.dart
    
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>
    
    * Update lib/tts_isolate.dart
    
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>
    
    * refactor: clean up code/string changes
    
    * refactor: remove more un-needed code
    
    * fix: failing tests
    
    * refactor: re add too many messages for queue length
    
    * refactor: remove unused function
    
    * refactor: revert this back to just context
    
    * chore: remove unused/commented out code
    
    ---------
    
    Co-authored-by: Jessie Wilson <48299282+jlw4049@users.noreply.github.com>
    Co-authored-by: jlw4049 <jlw_4049@hotmail.com>
    Co-authored-by: Kevin Wang <kevmo314@gmail.com>

--- lib/tts_isolate.dart
+++ lib/tts_isolate.dart
@@ -5,95 +5,43 @@ import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:rtchat/tts_plugin.dart';
 
-final ttsQueue = TTSQueue();
 final DateTime ttsTimeStampListener = DateTime.now();
 StreamSubscription? messagesSubscription;
 StreamSubscription? channelSubscription;
 
 @pragma("vm:entry-point")
-void isolateMain(SendPort sendPort) {
-  sendPort.send("Isolate message");
-}
-
-void initializeService() async {
-  final service = FlutterBackgroundService();
-
-  // this will be used as notification channel id
-  const notificationChannelId = 'my_foreground';
-
-  // this will be used for notification id, So you can update your custom notification with this id.
-  const notificationId = 888;
-
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-        onStart: onStart,
-        autoStart: true,
-        isForegroundMode: false,
-        notificationChannelId: notificationChannelId,
-        foregroundServiceNotificationId: notificationId),
-    iosConfiguration: IosConfiguration(
-      autoStart: true,
-      onForeground: onStart,
-    ),
-  );
-}
-
-@pragma('vm:entry-point')
-Future<bool> onIosBackground(ServiceInstance service) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  DartPluginRegistrant.ensureInitialized();
-
-  return true;
-}
-
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) async {
+Future<void> isolateMain(
+    SendPort sendPort, StreamController<String> channelStream) async {
   DartPluginRegistrant.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  final ttsQueue = TTSQueue();
 
-  channelSubscription = service.on("setTtsChannel").listen((event) async {
-    if (event != null) {
-      Map<String, dynamic>? args = event;
-      if (args['channel'] == null) {
-        await ttsQueue.clear();
-        messagesSubscription?.cancel();
-      } else {
-        final channelData = args["channel"];
-        var ttsCurrentChannel =
-            "${channelData['provider']}:${channelData['channelId']}";
-        messagesSubscription?.cancel();
-        messagesSubscription = FirebaseFirestore.instance
-            .collection('channels')
-            .doc(ttsCurrentChannel)
-            .collection('messages')
-            .where('timestamp', isGreaterThan: ttsTimeStampListener)
-            .orderBy('timestamp', descending: true)
-            .limit(1)
-            .snapshots()
-            .listen((latestMessage) async {
-          if (latestMessage.docs.isNotEmpty) {
-            final textToSpeak = latestMessage.docs[0]['message'] as String?;
-            if (textToSpeak != null) {
-              await ttsQueue.speak(latestMessage.docs[0].id, textToSpeak);
-            }
+  // Listen for changes to the stream
+  channelSubscription = channelStream.stream.listen((currentChannel) async {
+    if (currentChannel.isEmpty) {
+      await ttsQueue.clear();
+      messagesSubscription?.cancel();
+    } else {
+      messagesSubscription?.cancel();
+      messagesSubscription = FirebaseFirestore.instance
+          .collection('channels')
+          .doc(currentChannel)
+          .collection('messages')
+          .where('timestamp', isGreaterThan: ttsTimeStampListener)
+          .orderBy('timestamp', descending: true)
+          .limit(1)
+          .snapshots()
+          .listen((latestMessage) async {
+        if (latestMessage.docs.isNotEmpty) {
+          final textToSpeak = latestMessage.docs[0]['message'] as String?;
+          if (textToSpeak != null) {
+            await ttsQueue.speak(latestMessage.docs[0].id, textToSpeak);
           }
-        });
-      }
+        }
+      });
     }
   });
-
-  /// you can see this log in logcat
-  debugPrint('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
-
-  // test using external plugin
-  service.invoke(
-    'update',
-    {
-      "current_date": DateTime.now().toIso8601String(),
-    },
-  );
 }
