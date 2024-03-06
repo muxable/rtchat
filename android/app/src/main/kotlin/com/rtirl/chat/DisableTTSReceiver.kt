c6dc75b42b9ca1eb5290b45783a31e94d7bf6944
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-03-06 02:59:42 -0800

    Update DisableTTSReceiver.kt

--- android/app/src/main/kotlin/com/rtirl/chat/DisableTTSReceiver.kt
+++ android/app/src/main/kotlin/com/rtirl/chat/DisableTTSReceiver.kt
@@ -1,9 +1,9 @@
 package com.rtirl.chat
 
-import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.app.NotificationManager
 import android.speech.tts.TextToSpeech
 import com.rtirl.chat.TextToSpeechSingleton
 
