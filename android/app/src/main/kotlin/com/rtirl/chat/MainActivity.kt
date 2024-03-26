299cce651f0228025d908cb5e2f1b08b247197fd
jlw4049 <jlw_4049@hotmail.com>
2024-03-26 15:39:18 -0400

    feat: add a method to update pitch & speed to the native tts code

--- android/app/src/main/kotlin/com/rtirl/chat/MainActivity.kt
+++ android/app/src/main/kotlin/com/rtirl/chat/MainActivity.kt
@@ -169,6 +169,13 @@ class TextToSpeechPlugin(context: Context) : MethodCallHandler {
         Log.d("TextToSpeechPlugin", call.method)
 
         when (call.method) {
+            "updateTTSPreferences" -> {
+                val pitch = call.argument<Double?>("pitch")
+                val speed = call.argument<Double?>("speed")
+                if (pitch != null && speed != null) {
+                    updateTTSPreferences(pitch.toFloat(), speed.toFloat())
+                }
+            }
             "speak" -> {
                 val text = call.argument<String>("text")
                 if (!text.isNullOrBlank()) {
@@ -190,6 +197,11 @@ class TextToSpeechPlugin(context: Context) : MethodCallHandler {
             }
             else -> result.notImplemented()
         }
+    }
+
+    fun updateTTSPreferences(pitch: Float, speed: Float) {
+        tts.setPitch(pitch)
+        tts.setSpeechRate(speed)
     }
 
     fun speak(text: String, result: Result) {
