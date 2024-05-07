7d8ebc48e55c575a34287db1679e28812f82d29c
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-05-07 02:50:06 -0700

    fix: merge

--- lib/models/adapters/profiles.dart
+++ lib/models/adapters/profiles.dart
@@ -47,4 +47,13 @@ class ProfilesAdapter {
       return doc.exists && (doc.get("claims")['ads'] ?? false);
     });
   }
+
+  Future<String> getCompanionAuthToken({required String sessionUuid}) async {
+    final doc = await db
+        .collection("companion-tokens")
+        .doc(sessionUuid)
+        .snapshots()
+        .firstWhere((event) => event.exists);
+    return doc.data()!["token"] as String;
+  }
 }
