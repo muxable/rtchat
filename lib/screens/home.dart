9ad8819e075e793e2504afd77e4ee519d07603f7
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-05-10 00:54:41 -0700

    fix: anaylzer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
