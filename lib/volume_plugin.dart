f209c9c4e7a275d15cbb134fb8713cc29f9f7de7
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-06-10 00:05:57 -0700

    fix: plugin

--- lib/volume_plugin.dart
+++ lib/volume_plugin.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/services.dart';
 
 class VolumePlugin {
-  static const MethodChannel _channel = MethodChannel('volume_channel');
+  static const MethodChannel channel = MethodChannel('volume_channel');
 
   static Future<void> reduceVolumeOnTtsStart() async {
-    await _channel.invokeMethod('tts_on');
+    await channel.invokeMethod('tts_on');
   }
 
   static Future<void> increaseVolumeOnTtsStop() async {
-    await _channel.invokeMethod('tts_off');
+    await channel.invokeMethod('tts_off');
   }
 }
