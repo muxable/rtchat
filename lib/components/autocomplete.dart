728456f90e2e86d7dc69d55117c6300f20691e09
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-09-08 23:57:53 -0700

    fix: empty statement

--- lib/components/autocomplete.dart
+++ lib/components/autocomplete.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -120,7 +119,6 @@ class _AutocompleteWidgetState extends State<AutocompleteWidget> {
                           image: ResilientNetworkImage(emote.uri)));
                 }).toList(),
               );
-              ;
             });
           },
         );
