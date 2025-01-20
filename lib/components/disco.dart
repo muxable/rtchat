4c0580a20f9e4fd9be304cb0e9ed7ed42074e527
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2025-01-19 22:26:21 -0800

    fix: opacity migration

--- lib/components/disco.dart
+++ lib/components/disco.dart
@@ -3,13 +3,13 @@ import 'package:provider/provider.dart';
 import 'package:rtchat/models/style.dart';
 
 final discoModeColors = [
-  Colors.red.withOpacity(0.7),
-  Colors.blue.withOpacity(0.7),
-  Colors.green.withOpacity(0.7),
-  Colors.purple.withOpacity(0.7),
-  Colors.yellow.withOpacity(0.7),
-  Colors.cyan.withOpacity(0.7),
-  Colors.brown.withOpacity(0.7),
+  Colors.red.withValues(alpha: 0.7),
+  Colors.blue.withValues(alpha: 0.7),
+  Colors.green.withValues(alpha: 0.7),
+  Colors.purple.withValues(alpha: 0.7),
+  Colors.yellow.withValues(alpha: 0.7),
+  Colors.cyan.withValues(alpha: 0.7),
+  Colors.brown.withValues(alpha: 0.7),
 ];
 
 class DiscoWidget extends StatelessWidget {
