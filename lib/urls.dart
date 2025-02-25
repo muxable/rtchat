33fe68a3a4c68b97e06820d22b3babc6d0c2aa8a
Bill Odida <billodida420@gmail.com>
2025-02-25 17:23:17 +0300

    fix:corrcting close buttons

--- lib/urls.dart
+++ lib/urls.dart
@@ -3,34 +3,30 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openUrl(Uri url) async {
   if (!url.hasScheme) {
-    await customtabs.launchUrl(url.replace(scheme: 'http'),
+    await customtabs.launchUrl(
+      url.replace(scheme: 'http'),
       customTabsOptions: customtabs.CustomTabsOptions(
         shareState: customtabs.CustomTabsShareState.on,
         urlBarHidingEnabled: true,
         showTitle: true,
-        closeButton: customtabs.CustomTabsCloseButton(
-          icon: customtabs.CustomTabsCloseButtonIcons.back,
-        ),
       ),
       safariVCOptions: customtabs.SafariViewControllerOptions(
         barCollapsingEnabled: true,
-        dismissButtonStyle: customtabs.SafariViewControllerDismissButtonStyle.close,
+        dismissButtonStyle:
+            customtabs.SafariViewControllerDismissButtonStyle.close,
       ),
     );
   } else if (url.isScheme("http") || url.isScheme("https")) {
-    await customtabs.launchUrl(url,
+    await customtabs.launchUrl(
+      url,
       customTabsOptions: customtabs.CustomTabsOptions(
         shareState: customtabs.CustomTabsShareState.on,
         urlBarHidingEnabled: true,
         showTitle: true,
-        closeButton: customtabs.CustomTabsCloseButton(
-          icon: customtabs.CustomTabsCloseButtonIcons.back,
-        ),
       ),
       safariVCOptions: customtabs.SafariViewControllerOptions(
         barCollapsingEnabled: true,
-        dismissButtonStyle: customtabs.SafariViewControllerDismissButtonStyle.close,
-      ),
+      )
     );
   } else {
     await launchUrl(url);
