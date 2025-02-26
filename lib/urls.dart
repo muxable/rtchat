25d17f1acdfcd834841ce940f9c974e2b7c5b0a4
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2025-02-26 15:34:02 -0800

    fix: url package (#1334)
    * fix: url package
    
    * fix: import from 1333
    
    Co-authored-by: Bill Odida <billodida420@gmail.com>
    
    * fix: constructors
    
    * fix: upgrade web auth package
    
    ---------
    
    Co-authored-by: Bill Odida <billodida420@gmail.com>

--- lib/urls.dart
+++ lib/urls.dart
@@ -3,9 +3,29 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openUrl(Uri url) async {
   if (!url.hasScheme) {
-    await customtabs.launch(url.replace(scheme: 'http').toString());
+    await customtabs.launchUrl(
+      url.replace(scheme: 'http'),
+      customTabsOptions: const customtabs.CustomTabsOptions(
+        shareState: customtabs.CustomTabsShareState.on,
+        urlBarHidingEnabled: true,
+        showTitle: true,
+      ),
+      safariVCOptions: const customtabs.SafariViewControllerOptions(
+        barCollapsingEnabled: true,
+        dismissButtonStyle:
+            customtabs.SafariViewControllerDismissButtonStyle.close,
+      ),
+    );
   } else if (url.isScheme("http") || url.isScheme("https")) {
-    await customtabs.launch(url.toString());
+    await customtabs.launchUrl(url,
+        customTabsOptions: const customtabs.CustomTabsOptions(
+          shareState: customtabs.CustomTabsShareState.on,
+          urlBarHidingEnabled: true,
+          showTitle: true,
+        ),
+        safariVCOptions: const customtabs.SafariViewControllerOptions(
+          barCollapsingEnabled: true,
+        ));
   } else {
     await launchUrl(url);
   }
